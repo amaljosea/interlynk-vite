9d0eb66c2f15e9dd47bf02587c18ccab308d04fe
Surendra Pathak <Surendra.Pathak@interlynk.io>
2023-07-26 00:31:18 -0700

    [NO-TKT] Fix crash with non-semver tag

--- src/views/Dashboard/SBOMs/index.js
+++ src/views/Dashboard/SBOMs/index.js
@@ -102,7 +102,12 @@ function SBOMs() {
         } else if (b.name === 'latest') {
           return 1;
         }
-        return semver.compare(semver.coerce(b.name), semver.coerce(a.name));
+        var coerced_a = semver.valid(semver.coerce(a.name));
+        var coerced_b = semver.valid(semver.coerce(b.name));
+        if (coerced_a === null || coerced_b === null) {
+          return b.name.localeCompare(a.name);
+        }
+        return semver.compare(coerced_b, coerced_a);
       });
 
 
