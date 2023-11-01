e57f8a709870f907059641b755f18604aa51bf7e
abhisekdev <abhisek@sofueled.com>
2023-11-01 16:16:36 +0530

    add new drawer for component relationship ui

---
+++ src/components/Drawer/RelationshipDrawer.js
@@ -0,0 +1,263 @@
+import { AddIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  FormControl,
+  FormLabel,
+  IconButton,
+  Select,
+  Stack,
+  Table,
+  Tag,
+  TagCloseButton,
+  TagLabel,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tooltip,
+  Tr
+} from '@chakra-ui/react'
+import Card from 'components/Card/Card'
+import CardBody from 'components/Card/CardBody'
+import CardHeader from 'components/Card/CardHeader'
+import { useState } from 'react'
+
+const RelationshipDrawer = ({ isOpen, onClose, data }) => {
+  const { name, version } = data
+
+  const [createRelation, setCreateRelation] = useState(false)
+  const [relation, setRelation] = useState('')
+  const [component, setComponent] = useState('')
+
+  const [dependencyOf, setDependencyOf] = useState([
+    'Microsoft.Graph.Core 2.0.8'
+  ])
+
+  const [dependsOn, setDependsOn] = useState([
+    'Microsoft.Bcl.AsyncInterfaces 1.1.1',
+    'System.Diagnostics.DiagnosticSource 6.0.0',
+    'System.Memory.Data 1.0.2'
+  ])
+
+  const handleSubmit = () => {
+    if (relation === 'Dependency Of') {
+      setDependencyOf((prevData) => [component, ...prevData])
+    }
+    if (relation === 'Depends On') {
+      setDependsOn((prevData) => [component, ...prevData])
+    }
+
+    setCreateRelation(false)
+  }
+
+  const handleRemove = (relation, comp) => {
+    if (relation === 'Dependency Of') {
+      const compData = dependencyOf.filter((_, index) => index !== comp)
+      setDependencyOf(compData)
+    }
+    if (relation === 'Depends On') {
+      const compData = dependsOn.filter((_, index) => index !== comp)
+      setDependsOn(compData)
+    }
+  }
+
+  return (
+    <Drawer
+      size='lg'
+      isOpen={isOpen}
+      placement='right'
+      onClose={onClose}
+      closeOnOverlayClick={true}
+    >
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton />
+        <DrawerHeader borderBottomWidth='1px' color='gray.600'>
+          Edit Relationship
+        </DrawerHeader>
+        <DrawerBody>
+          <Card px={0} mx={0}>
+            <CardHeader>
+              <Stack
+                width='100%'
+                direction={'row'}
+                alignItems={'center'}
+                justifyContent={'space-between'}
+              >
+                <Text fontSize={'lg'} fontWeight={'medium'}>
+                  {name} - {version}
+                </Text>
+                <Tooltip placement='left' label='Add Relationship'>
+                  <IconButton
+                    icon={<AddIcon />}
+                    colorScheme='blue'
+                    variant='solid'
+                    fontWeight='normal'
+                    size='md'
+                    onClick={() => {
+                      setCreateRelation(true)
+                      setRelation('')
+                      setComponent('')
+                    }}
+                  />
+                </Tooltip>
+              </Stack>
+            </CardHeader>
+            <CardBody>
+              <Flex
+                flexDir={'column'}
+                alignItems={'flex-start'}
+                width={'100%'}
+                gap={4}
+              >
+                {/* CREATE RELATIONSHIP */}
+                {createRelation && (
+                  <Stack
+                    width={'100%'}
+                    direction={'column'}
+                    alignItems={'flex-start'}
+                    gap={2}
+                    mt={6}
+                  >
+                    <FormControl>
+                      <FormLabel htmlFor='relation' color='gray.600'>
+                        Relation
+                      </FormLabel>
+                      <Select
+                        id='relation'
+                        size='sm'
+                        value={relation}
+                        onChange={(e) => setRelation(e.target.value)}
+                      >
+                        <option value=''>-- Select --</option>
+                        {['Dependency Of', 'Depends On'].map((item, idx) => (
+                          <option key={idx} value={item.relation}>
+                            {item}
+                          </option>
+                        ))}
+                      </Select>
+                    </FormControl>
+                    <FormControl>
+                      <FormLabel htmlFor='component' color='gray.600'>
+                        Component
+                      </FormLabel>
+                      <Select
+                        id='component'
+                        size='sm'
+                        value={component}
+                        onChange={(e) => setComponent(e.target.value)}
+                      >
+                        <option value=''>-- Select --</option>
+                        {[
+                          'System.Xml.XDocument',
+                          'System.Xml.ReaderWriter',
+                          'System.Windows.Extensions',
+                          'System.Text.Encoding',
+                          'System.Text.Encodings.Web'
+                        ].map((item, idx) => (
+                          <option key={idx} value={item}>
+                            {item}
+                          </option>
+                        ))}
+                      </Select>
+                    </FormControl>
+                    <Button
+                      size='md'
+                      mt={2}
+                      width={'fit-content'}
+                      colorScheme='blue'
+                      onClick={handleSubmit}
+                    >
+                      Add
+                    </Button>
+                  </Stack>
+                )}
+                {/* COMONENT RELATIONSIP DATA */}
+                <Table mt={6} width={'100%'}>
+                  <Thead>
+                    <Tr>
+                      {['Relation', 'Component'].map((item, index) => (
+                        <Th key={index} pl={0} width={'100px'}>
+                          <Box>{item}</Box>
+                        </Th>
+                      ))}
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {/* DEPENDENCY OF */}
+                    <Tr>
+                      <Td pl={0}>
+                        <Text fontSize='xs' fontWeight={'medium'}>
+                          Dependency Of
+                        </Text>
+                      </Td>
+                      <Td pl={0}>
+                        <Stack direction={'column'}>
+                          {dependencyOf.map((comp, index) => (
+                            <Tag
+                              size={'sm'}
+                              key={index}
+                              variant='subtle'
+                              colorScheme='blue'
+                              width={'fit-content'}
+                            >
+                              <TagLabel>{comp}</TagLabel>
+                              <TagCloseButton
+                                onClick={() =>
+                                  handleRemove('Dependency Of', index)
+                                }
+                              />
+                            </Tag>
+                          ))}
+                        </Stack>
+                      </Td>
+                    </Tr>
+                    {/* DEPENDS ON */}
+                    <Tr>
+                      <Td pl={0}>
+                        <Text fontSize='xs' fontWeight={'medium'}>
+                          Depends On
+                        </Text>
+                      </Td>
+                      <Td pl={0}>
+                        <Stack direction={'column'}>
+                          {dependsOn.map((comp, index) => (
+                            <Tag
+                              size={'sm'}
+                              key={index}
+                              variant='subtle'
+                              colorScheme='blue'
+                              width={'fit-content'}
+                            >
+                              <TagLabel>{comp}</TagLabel>
+                              <TagCloseButton
+                                onClick={() =>
+                                  handleRemove('Depends On', index)
+                                }
+                              />
+                            </Tag>
+                          ))}
+                        </Stack>
+                      </Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </Flex>
+            </CardBody>
+          </Card>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export default RelationshipDrawer
