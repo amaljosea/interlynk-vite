769c594f798fe01a53178de003ee7d2b5e875b97
abhisekdev <abhisek@sofueled.com>
2023-12-13 10:48:28 +0530

    add toast message for unique identifier check

--- src/components/Tables/HealthCheckTable.js
+++ src/components/Tables/HealthCheckTable.js
@@ -372,18 +372,29 @@ const HealthCheckTable = ({
         sbomId: sbomId,
         uniqueId: true
       }
-    }).then((res) => {
-      if (res.data) {
-        setPageIndex(1)
-        healthRecheck({
-          variables: {
-            checkId: row.organizationRule.rule.friendlyId,
-            compId: row.componentId,
-            sbomId: sbomId
-          }
-        })
-      }
     })
+      .then((res) => {
+        if (res.data) {
+          setPageIndex(1)
+          healthRecheck({
+            variables: {
+              checkId: row.organizationRule.rule.friendlyId,
+              compId: row.componentId,
+              sbomId: sbomId
+            }
+          })
+        }
+      })
+      .finally(() => {
+        setTimeout(() => {
+          toast({
+            description: 'A unique identifier has been added to the component',
+            status: 'success',
+            duration: 3000,
+            position: 'top'
+          })
+        }, 1000)
+      })
   }
 
   // CHECK UNIQUE IDENTIFIER
