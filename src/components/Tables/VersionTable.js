f324d986cd31261647164bb70bf37b07aaaede25
abhisekdev <abhisek@sofueled.com>
2023-12-21 10:12:50 +0530

    fixed version tab pagination

--- src/components/Tables/VersionTable.js
+++ src/components/Tables/VersionTable.js
@@ -62,7 +62,7 @@ const VersionTable = ({ name, project, productId, refetch, getVulnData }) => {
 
   const data = project ? removeDuplicates(project.sboms) : []
 
-  const totalPages = data && Math.ceil(data.length / totalRows)
+  const totalPages = data.length > 0 ? Math.ceil(data.length / totalRows) : 1
 
   const filteredData =
     data && data.slice((currentPage - 1) * totalRows, currentPage * totalRows)
