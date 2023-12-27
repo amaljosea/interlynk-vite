3ee2ce0855b7a4cb59e00b0abb46a63a2c61daba
Surendra Pathak <Surendra.Pathak@interlynk.io>
2023-12-26 19:34:26 -0800

    [NO-TKT] Remove time selector for metric

--- src/components/Navbars/AdminNavbarLinks.js
+++ src/components/Navbars/AdminNavbarLinks.js
@@ -146,7 +146,7 @@ export default function HeaderLinks(props) {
           <option value='3 weeks'>3 weeks</option>
           <option value='1 month'>1 month</option>
         </Select>
-      ) */}
+        ) */}
       {productId && !customerView && (
         <Popover isLazy>
           <PopoverTrigger>
