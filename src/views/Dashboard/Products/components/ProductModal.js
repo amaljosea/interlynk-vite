5174c89d865249a0b0d638241081791e801249ce
abhisekdev <abhisek@sofueled.com>
2023-12-27 18:00:18 +0530

    fixed product update function with required changes

--- src/views/Dashboard/Products/components/ProductModal.js
+++ src/views/Dashboard/Products/components/ProductModal.js
@@ -63,6 +63,7 @@ const ProductModal = ({
           'A project with same name already exists. Please choose an unique name'
         )
       } else {
+        navigate(`/vendor/products/${productName}?id=${id}`)
         onClose()
       }
     })
