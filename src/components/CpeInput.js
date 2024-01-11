1579ceaf73fb73b27429305b13af58a8d7065980
abhisekdev <abhisek@sofueled.com>
2024-01-11 18:30:31 +0530

    update cpe editor with new fields

--- src/components/CpeInput.js
+++ src/components/CpeInput.js
@@ -13,6 +13,7 @@ import {
 import { CheckIcon, WarningTwoIcon } from '@chakra-ui/icons'
 import { PackageURL } from 'packageurl-js'
 import { useGlobalState } from 'hooks/useGlobalState'
+import { validateCpe } from 'utils'
 
 const regexPattern =
   /cpe:2\.3:[aho\*\-](:(((\?*|\*?)([a-zA-Z0-9\-\._]|(\\[\\\*\?!"#$$%&'\(\)\+,\/:;<=>@\[\]\^`\{\|}~]))+(\?*|\*?))|[\*\-])){5}(:(([a-zA-Z]{2,3}(-([a-zA-Z]{2}|[0-9]{3}))?)|[\*\-]))(:(((\?*|\*?)([a-zA-Z0-9\-\._]|(\\[\\\*\?!"#$$%&'\(\)\+,\/:;<=>@\[\]\^`\{\|}~]))+(\?*|\*?))|[\*\-])){4}/
@@ -122,16 +123,17 @@ const CpeInput = ({
     listItemsRef.current[0]?.focus()
   }, [])
 
-  useEffect(() => {
-    if (validation) {
-      const matches = regexPattern.test(inputValue)
-      if (matches) {
-        prodCompDispatch({ type: 'SET_CPE_VALIDATION', payload: true })
-      } else {
-        prodCompDispatch({ type: 'SET_CPE_VALIDATION', payload: false })
-      }
-    }
-  }, [inputValue])
+  // useEffect(() => {
+  //   if (validation) {
+  //     const matches = validateCpe(inputValue)
+  //     console.log('matches', matches)
+  //     if (matches) {
+  //       prodCompDispatch({ type: 'SET_CPE_VALIDATION', payload: true })
+  //     } else {
+  //       prodCompDispatch({ type: 'SET_CPE_VALIDATION', payload: false })
+  //     }
+  //   }
+  // }, [inputValue])
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -163,8 +165,8 @@ const CpeInput = ({
             {name === 'packageName'
               ? 'Package Name'
               : name === 'packageVersion'
-              ? 'Version'
-              : name}
+                ? 'Version'
+                : name}
           </FormLabel>
         )}
         <InputGroup>
