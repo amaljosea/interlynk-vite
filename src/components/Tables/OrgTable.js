9e4c421313d867dae362133862e37f1739633c12
Surendra Pathak <Surendra.Pathak@interlynk.io>
2024-01-17 23:47:41 -0800

    [NO-TKT Display string update

--- src/components/Tables/OrgTable.js
+++ src/components/Tables/OrgTable.js
@@ -198,7 +198,7 @@ const OrgTable = ({ data, refetch, activeOrg }) => {
     },
     {
       id: 'CONTACT_EMAIL',
-      name: 'CONTACT EMAIL',
+      name: 'CONTACT',
       selector: (row) => {
         const { email } = row
         return <Text fontSize={14}>{email}</Text>
