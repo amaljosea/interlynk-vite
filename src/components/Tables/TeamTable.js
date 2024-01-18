e37adbcc4706a5cb1e990d133bc19e190482f3df
abhisekdev <abhisek@sofueled.com>
2024-01-18 12:53:33 +0530

    update user status string format

--- src/components/Tables/TeamTable.js
+++ src/components/Tables/TeamTable.js
@@ -169,7 +169,9 @@ const TeamTable = ({ data, refetch }) => {
             }
             textTransform={'capitalize'}
           >
-            <TagLabel mx='auto'>{invitationStatus}</TagLabel>
+            <TagLabel mx='auto'>
+              {invitationStatus?.replace(/_/g, ' ')}
+            </TagLabel>
           </Tag>
         )
       }
