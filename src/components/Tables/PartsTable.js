23c7914f72491b5b148b35df5bd65b0536dd97c1
sijintv <sijintv1@gmail.com>
2024-01-23 17:14:50 +0530

    Add refresh buttons to all the tables

--- src/components/Tables/PartsTable.js
+++ src/components/Tables/PartsTable.js
@@ -1,5 +1,5 @@
 import { useLazyQuery, useMutation, useQuery } from '@apollo/client'
-import { AddIcon } from '@chakra-ui/icons'
+import {AddIcon, RepeatIcon} from '@chakra-ui/icons'
 import {
   Button,
   Flex,
@@ -478,6 +478,15 @@ const PartsTable = ({ data, refetch, getVulnData, getCompData }) => {
   // CLEAR SERACH
   const handleClear = () => setSearchInput('')
 
+  const handleRefresh = async () => {
+    await refetch({
+      variables: {
+        projectId: prodId,
+        sbomId: sbomId
+      }
+    })
+  }
+
   // SUB HEADER
   const subHeaderComponent = useMemo(() => {
     return (
@@ -535,6 +544,13 @@ const PartsTable = ({ data, refetch, getVulnData, getCompData }) => {
               fontWeight='normal'
               fontSize={'sm'}
             />
+          </Tooltip>
+          <Tooltip label='Refresh'>
+            <IconButton
+                onClick={handleRefresh}
+                colorScheme='blue'
+                icon={<RepeatIcon />}
+            ></IconButton>
           </Tooltip>
         </Stack>
       </Flex>
