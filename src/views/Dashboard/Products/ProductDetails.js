3312fe5bd6dae963dcd0273b3ff5af5a4521da80
abhisekdev <abhisek@sofueled.com>
2024-01-23 22:11:27 +0530

    update style

--- src/views/Dashboard/Products/ProductDetails.js
+++ src/views/Dashboard/Products/ProductDetails.js
@@ -472,7 +472,7 @@ const ProductDetails = () => {
                 >
                   {defaultEnv}
                 </MenuButton>
-                <MenuList height={'250px'} overflowY={'scroll'}>
+                <MenuList minH={'auto'} maxH={'250px'} overflowY={'scroll'}>
                   <MenuOptionGroup
                     type='radio'
                     value={activeEnv}
