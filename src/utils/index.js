2e8c00f3fc61da1dc60d3140a835de4f12c4cc1b
abhisekdev <abhisek@sofueled.com>
2024-01-24 19:02:58 +0530

    update parts create logic

--- src/utils/index.js
+++ src/utils/index.js
@@ -599,14 +599,11 @@ export const customStyles = {
 }
 
 export const normalizeSBOMVersion = (sbom) => {
-  if (sbom?.primaryComponent?.version) {
+  if (sbom?.primaryComponent) {
     return sbom.primaryComponent.version
-  } else if (sbom?.primaryComponent?.name) {
-    return sbom.primaryComponent.name
-  } else if (sbom?.creationAt) {
-    return 'Uploaded: ' + getFullDateAndTime(sbom?.creationAt)
+  } else {
+    return `Uploaded ${getFullDateAndTime(sbom?.creationAt)}`
   }
-  return ''
 }
 
 // REMOVE DUPLICATE PRODUCTS VERSIONS
