29d0c940169d1445a3c404f4de06055efe77f45a
sijintv <sijintv1@gmail.com>
2024-02-23 20:59:20 +0530

    Fix pagination on initial load

--- src/components/Licenses/index.js
+++ src/components/Licenses/index.js
@@ -9,7 +9,8 @@ const Licenses = () => {
         fetchPolicy: 'network-only',
         variables: {
             direction: 'ASC',
-            status: "unspecified"
+            status: "unspecified",
+            first: 25
         }
     })
 
