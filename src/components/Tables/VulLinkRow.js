e2cd91145ae5589d887b0c2ef7df5cd493da1ba6
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-04-03 18:39:25 +0530

    Fixed vex note alignment issue (#2662)

--- src/components/Tables/VulLinkRow.js
+++ src/components/Tables/VulLinkRow.js
@@ -16,8 +16,12 @@ const VulLinkRow = ({ username, justification, status, timestamp, note, impact, 
         </Flex>
       </Td>
       <Td fontSize={'xs'} pl={0}>{justification}</Td>
-      <Td fontSize={'xs'} pl={0}>{impact && <Tooltip text={impact}><BiNote /></Tooltip>}</Td>
-      <Td fontSize={'xs'} pl={0}>{note && <Tooltip text={note}><BiNote /></Tooltip>}</Td>
+      <Td fontSize={'xs'} pl={0}>
+        {impact && <Tooltip text={impact?.length > 35 ? `${impact.substring(0, 35)}...` : impact}><BiNote /></Tooltip>}
+      </Td>
+      <Td fontSize={'xs'} pl={0}>
+        {note && <Tooltip text={note?.length > 35 ? `${note.substring(0, 35)}...` : note}><BiNote /></Tooltip>}
+      </Td>
       <Td fontSize={'xs'} pl={0}>{username}</Td>
       <Td fontSize={'xs'} pl={0}><Tooltip text={getFullDateAndTime(timestamp)}>{timeSince(timestamp)}</Tooltip></Td>
     </Tr>
