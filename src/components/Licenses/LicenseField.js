b9e8df899ab4e16b8cbc8737b122bf71f6015700
Amal Jose Alex <howareyouami@gmail.com>
2024-04-05 15:57:08 +0530

    Try isClearable (#2718)
    Co-authored-by: Amal Jose <amaljoseami@gmail.com>

--- src/components/Licenses/LicenseField.js
+++ src/components/Licenses/LicenseField.js
@@ -1,47 +1,50 @@
+import { useLazyQuery } from '@apollo/client'
+import { useEffect, useState } from 'react'
+import ReactSelect, { components } from 'react-select'
+
 import { InfoIcon } from '@chakra-ui/icons'
 import {
   Flex,
   FormControl,
   FormLabel,
-  Tooltip,
   Icon,
+  Tag,
+  TagLabel,
   Text,
+  Tooltip,
   VStack,
-  useDisclosure, TagLabel, Tag
+  useDisclosure
 } from '@chakra-ui/react'
-import { useState, useEffect } from 'react'
-import ReactSelect, { components } from 'react-select';
+
+import { useGlobalState } from 'hooks/useGlobalState'
 
 import { LicenseAutoComplete } from 'graphQL/Queries'
 
-import { useLazyQuery } from '@apollo/client'
-import { useGlobalState } from 'hooks/useGlobalState'
 import InfoModal from '../InfoModal'
 
-
-const LicenseField = ({ isValid, setIsValid, isDisabled, sbomView, license }) => {
-
+const LicenseField = ({ isDisabled, sbomView, license }) => {
   useEffect(() => {
     if (license) {
       const payload = {
         value: license,
-        label: license,
+        label: license
       }
       dispatcher({ type: 'SET_LICENSE_FIELD', payload: [payload] })
     }
     return () => {
-      dispatcher({ type: 'SET_LICENSE_FIELD', payload: [] }); // clear license field while unmounting
-    };
+      dispatcher({ type: 'SET_LICENSE_FIELD', payload: [] }) // clear license field while unmounting
+    }
   }, [])
 
   const { prodCompState, activeSbomTab, dispatch, sbomState } = useGlobalState()
 
   const { prodCompDispatch, sbomDispatch } = dispatch
 
-  const licenseString = sbomView ? sbomState.licenseString : prodCompState.licenseString
+  const licenseString = sbomView
+    ? sbomState.licenseString
+    : prodCompState.licenseString
 
   const dispatcher = sbomView ? sbomDispatch : prodCompDispatch
-
 
   const [licenseList, setLicenseList] = useState([])
   const [licenseType, setLicenseType] = useState('')
@@ -50,12 +53,17 @@ const LicenseField = ({ isValid, setIsValid, isDisabled, sbomView, license }) =>
   const [infoText, setInfoText] = useState('')
   const [infoUrl, setInfoUrl] = useState('')
 
-  const [getLicense] = useLazyQuery(LicenseAutoComplete, {  fetchPolicy: 'network-only' })
+  const [getLicense] = useLazyQuery(LicenseAutoComplete, {
+    fetchPolicy: 'network-only'
+  })
 
-  const { isOpen: isInfoOpen, onOpen: onInfoOpen, onClose: onInfoClose } = useDisclosure()
+  const {
+    isOpen: isInfoOpen,
+    onOpen: onInfoOpen,
+    onClose: onInfoClose
+  } = useDisclosure()
 
   const onLicenseChange = (selected) => {
-
     selected = [selected]
 
     dispatcher({ type: 'SET_LICENSE_FIELD', payload: selected })
@@ -73,12 +81,20 @@ const LicenseField = ({ isValid, setIsValid, isDisabled, sbomView, license }) =>
         }
       }).then((res) => {
         if (res.data) {
-          const licenses = res.data.licenseAutoComplete?.result?.map((license) => {
-            return { value: license.value, label: license.value, type: license.type}
-          })
+          const licenses = res.data.licenseAutoComplete?.result?.map(
+            (license) => {
+              return {
+                value: license.value,
+                label: license.value,
+                type: license.type
+              }
+            }
+          )
 
           //Remove duplicates values if any. Doing this since filtering over 25 entries is not expensive
-          const uniqueLicenses = licenses?.filter((v, i, a) => a.findIndex(t => (t.value === v.value)) === i)
+          const uniqueLicenses = licenses?.filter(
+            (v, i, a) => a.findIndex((t) => t.value === v.value) === i
+          )
 
           setLicenseList(uniqueLicenses)
         }
@@ -101,7 +117,7 @@ const LicenseField = ({ isValid, setIsValid, isDisabled, sbomView, license }) =>
     'SPDX License Expression': 'cyan',
     'Custom License Expression / Custom License': 'orange',
     'SPDX License ID': 'green',
-    'Custom License': 'orange',
+    'Custom License': 'orange'
   }
 
   const Option = (props) => {
@@ -113,7 +129,7 @@ const LicenseField = ({ isValid, setIsValid, isDisabled, sbomView, license }) =>
             width={'fit-content'}
             size={'sm'}
             variant='subtle'
-            colorScheme= {colorScheme[props.data.type]}
+            colorScheme={colorScheme[props.data.type]}
           >
             <TagLabel fontSize={'10px'}>{props.data.type}</TagLabel>
           </Tag>
@@ -125,19 +141,27 @@ const LicenseField = ({ isValid, setIsValid, isDisabled, sbomView, license }) =>
   return (
     <>
       <VStack spacing={4} alignItems={'flex-start'}>
-      <FormControl>
-        <FormLabel htmlFor={licenseType}>
-          <Flex flexDirection={'row'} alignItems={'center'} gap={2.5}>
-            <Text>Licenses</Text>
-            {activeSbomTab === 4 ? (
-              <Tooltip label={licenseInfo}><Icon as={InfoIcon} color={'blue.500'} /></Tooltip>
-            ) : (
-              <Icon as={InfoIcon} color={'blue.500'} cursor={'pointer'} onClick={onCheckLicense} />
-            )}
-          </Flex>
-        </FormLabel>
-        {/* LICENSE */}
+        <FormControl>
+          <FormLabel htmlFor={licenseType}>
+            <Flex flexDirection={'row'} alignItems={'center'} gap={2.5}>
+              <Text>Licenses</Text>
+              {activeSbomTab === 4 ? (
+                <Tooltip label={licenseInfo}>
+                  <Icon as={InfoIcon} color={'blue.500'} />
+                </Tooltip>
+              ) : (
+                <Icon
+                  as={InfoIcon}
+                  color={'blue.500'}
+                  cursor={'pointer'}
+                  onClick={onCheckLicense}
+                />
+              )}
+            </Flex>
+          </FormLabel>
+          {/* LICENSE */}
           <ReactSelect
+            isClearable
             isDisabled={isDisabled}
             styles={{
               control: (baseStyles, state) => {
@@ -165,18 +189,20 @@ const LicenseField = ({ isValid, setIsValid, isDisabled, sbomView, license }) =>
             placeholder={'Enter License'}
             className='react-select'
           />
-        {licenseType && <Flex justifyContent="flex-end">
-          <Tag
-            width={'fit-content'}
-            size={'sm'}
-            my={2}
-            variant='subtle'
-            colorScheme={colorScheme[licenseType]}
-          >
-            <TagLabel>{licenseType}</TagLabel>
-          </Tag>
-        </Flex>}
-      </FormControl>
+          {licenseType && (
+            <Flex justifyContent='flex-end'>
+              <Tag
+                width={'fit-content'}
+                size={'sm'}
+                my={2}
+                variant='subtle'
+                colorScheme={colorScheme[licenseType]}
+              >
+                <TagLabel>{licenseType}</TagLabel>
+              </Tag>
+            </Flex>
+          )}
+        </FormControl>
       </VStack>
 
       {/* INFO MODAL */}
@@ -190,7 +216,6 @@ const LicenseField = ({ isValid, setIsValid, isDisabled, sbomView, license }) =>
         />
       )}
     </>
-    
   )
 }
 
