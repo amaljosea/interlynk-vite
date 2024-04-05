b6e4eced81461053b9fcebf69ec372e8f753e428
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-04-05 14:27:11 +0530

    ✅ Implement policy stats view (#2717)
    * Implement policy stats view
    
    * Update code
    
    * Update code

--- src/views/Sbom/components/SbomDetails.js
+++ src/views/Sbom/components/SbomDetails.js
@@ -1,22 +1,61 @@
-import { Badge, Flex, HStack, Icon, IconButton, Stack, Tag, TagLabel, Text, Tooltip, useDisclosure } from '@chakra-ui/react'
-import VulnBadge from 'components/Misc/VulnBadge'
-import { useGlobalState } from 'hooks/useGlobalState'
-import { Link, useLocation } from 'react-router-dom'
-import { timeSince, getFullDateAndTime, parseJSONSafely } from 'utils'
-import { DownloadIcon, Search2Icon } from '@chakra-ui/icons'
-import { FaAngleLeft, FaBalanceScale, FaBug, FaCube, FaCubes } from 'react-icons/fa'
-import { FaCircleCheck } from 'react-icons/fa6'
-import { useEffect } from 'react'
 import { useLazyQuery } from '@apollo/client'
-import { GetPrimaryComponent } from 'graphQL/Queries'
+import { useEffect } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import { getFullDateAndTime, parseJSONSafely, timeSince } from 'utils'
+
+import { DownloadIcon, Search2Icon } from '@chakra-ui/icons'
+import {
+  Badge,
+  Flex,
+  HStack,
+  Icon,
+  IconButton,
+  Stack,
+  Tag,
+  TagLabel,
+  Text,
+  Tooltip,
+  useDisclosure
+} from '@chakra-ui/react'
+
 import GraphDrawer from 'components/Drawer/GraphDrawer'
+import VulnBadge from 'components/Misc/VulnBadge'
+
+import { useGlobalState } from 'hooks/useGlobalState'
+
+import { GetPrimaryComponent } from 'graphQL/Queries'
 import { GetSharPrimartComp } from 'graphQL/Queries'
 
+import {
+  FaAngleLeft,
+  FaBalanceScale,
+  FaBug,
+  FaCube,
+  FaCubes
+} from 'react-icons/fa'
+import { FaCircleCheck } from 'react-icons/fa6'
+import { MdPolicy } from 'react-icons/md'
+
 const SbomDetails = ({ sbom, getCompData, getVulnData }) => {
-  const { project, projectVersion, primaryComponent, updatedAt, lifecycle, vulnRunStatus, stats } = sbom
+  const {
+    project,
+    projectVersion,
+    primaryComponent,
+    updatedAt,
+    lifecycle,
+    vulnRunStatus,
+    stats
+  } = sbom
   const { compCount, compLicenseCount, vulnStats } = stats
   const { critical, high, medium, low, unknown } = vulnStats
-  const { totalRows, prodCompState, prodVulnState, setActiveSbomTab, setActiveCsSbomTab, dispatch } = useGlobalState()
+  const {
+    totalRows,
+    prodCompState,
+    prodVulnState,
+    setActiveSbomTab,
+    setActiveCsSbomTab,
+    dispatch
+  } = useGlobalState()
   const { field, direction } = prodCompState
   const { prodCompDispatch, prodVulnDispatch } = dispatch
 
@@ -49,10 +88,20 @@ const SbomDetails = ({ sbom, getCompData, getVulnData }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   // GET PRIMARY COMPONENT
-  const [getPrimaryComp, { data: primaryComp }] = useLazyQuery(signedUrlParams ? GetSharPrimartComp : GetPrimaryComponent,{ fetchPolicy: 'network-only' })
+  const [getPrimaryComp, { data: primaryComp }] = useLazyQuery(
+    signedUrlParams ? GetSharPrimartComp : GetPrimaryComponent,
+    { fetchPolicy: 'network-only' }
+  )
 
   const handleRelationView = async () => {
-    await getPrimaryComp({ variables: { projectId: signedUrlParams ? undefined : productId, sbomId: sbomId, primary: true, field, direction }
+    await getPrimaryComp({
+      variables: {
+        projectId: signedUrlParams ? undefined : productId,
+        sbomId: sbomId,
+        primary: true,
+        field,
+        direction
+      }
     }).then((res) => res?.data && onOpen())
   }
 
@@ -94,7 +143,18 @@ const SbomDetails = ({ sbom, getCompData, getVulnData }) => {
   const onFilterVuln = (value) => {
     const { field, direction } = prodVulnState
     prodVulnDispatch({ type: 'CLEAR_PROD_VULN' })
-    getVulnData({ projectId: signedUrlParams ? undefined : productId, sbomId: sbomId, source: undefined, severity: value || undefined, first: totalRows, last: undefined, after: undefined, before: undefined, field: field, direction: direction }).then((res) => {
+    getVulnData({
+      projectId: signedUrlParams ? undefined : productId,
+      sbomId: sbomId,
+      source: undefined,
+      severity: value || undefined,
+      first: totalRows,
+      last: undefined,
+      after: undefined,
+      before: undefined,
+      field: field,
+      direction: direction
+    }).then((res) => {
       if (res.data) {
         if (signedUrlParams) {
           setActiveCsSbomTab(3)
@@ -150,7 +210,14 @@ const SbomDetails = ({ sbom, getCompData, getVulnData }) => {
   return (
     <>
       <Flex direction={'row'} alignItems={'flex-start'} gap={5} width={'100%'}>
-        <Icon as={FaCubes} h={'64px'} w={'64px'} color='blue.300' onClick={handleRelationView} cursor={'pointer'} />
+        <Icon
+          as={FaCubes}
+          h={'64px'}
+          w={'64px'}
+          color='blue.300'
+          onClick={handleRelationView}
+          cursor={'pointer'}
+        />
         <Flex direction={'column'} gap={0.5}>
           {/* PRODUCT TITLE */}
           <Stack direction={'column'} spacing={1} alignItems={'left'}>
@@ -163,50 +230,114 @@ const SbomDetails = ({ sbom, getCompData, getVulnData }) => {
                   }}
                 >
                   <FaAngleLeft size={18} color='#3182CE' />
-                  <Text fontWeight={'semibold'} fontSize={18} color={'blue.500'} textDecor={'underline'} >
-                    {subProduct?.childFour ? subProduct?.childThree?.name : subProduct?.childThree ? subProduct?.childTwo?.name : subProduct?.childTwo ? subProduct?.childOne?.name : subProduct?.childOne ? subProduct?.name : name}
+                  <Text
+                    fontWeight={'semibold'}
+                    fontSize={18}
+                    color={'blue.500'}
+                    textDecor={'underline'}
+                  >
+                    {subProduct?.childFour
+                      ? subProduct?.childThree?.name
+                      : subProduct?.childThree
+                        ? subProduct?.childTwo?.name
+                        : subProduct?.childTwo
+                          ? subProduct?.childOne?.name
+                          : subProduct?.childOne
+                            ? subProduct?.name
+                            : name}
                   </Text>
                 </HStack>
               </Link>
             )}
-            <Flex direction={'row'} alignItems={'center'} flexWrap={'wrap'} gap={2} >
-              <Text fontWeight={'semibold'} fontSize={25}>{project?.projectGroup?.name}</Text>
+            <Flex
+              direction={'row'}
+              alignItems={'center'}
+              flexWrap={'wrap'}
+              gap={2}
+            >
+              <Text fontWeight={'semibold'} fontSize={25}>
+                {project?.projectGroup?.name}
+              </Text>
             </Flex>
           </Stack>
           <Flex flexDir={'row'} gap={1} alignItems={'center'} flexWrap={'wrap'}>
             <Text fontSize={22}>{primaryComponent?.name}</Text>
             <Text fontSize={22}>{primaryComponent?.version ? ':' : ''}</Text>
-            <Text mr={2} fontSize={22}>{projectVersion}</Text>
+            <Text mr={2} fontSize={22}>
+              {projectVersion}
+            </Text>
             <Tooltip label='Lifecycle stage' fontSize='md'>
-              <Tag w={'fit-content'} size={'sm'} variant='solid' colorScheme='blue' >
+              <Tag
+                w={'fit-content'}
+                size={'sm'}
+                variant='solid'
+                colorScheme='blue'
+              >
                 <TagLabel textTransform={'capitalize'}>{lifecycle}</TagLabel>
               </Tag>
             </Tooltip>
           </Flex>
-          <Text fontSize={'sm'} my={1}>{primaryComponent?.description}</Text>
+          <Text fontSize={'sm'} my={1}>
+            {primaryComponent?.description}
+          </Text>
           {/* SCAN STATUS */}
           {!signedUrlParams && (
-            <Flex flexDir='row' gap={2} alignItems={'center'} width='100%' my={2} >
+            <Flex
+              flexDir='row'
+              gap={2}
+              alignItems={'center'}
+              width='100%'
+              my={2}
+            >
               <Tooltip label='Imported'>
-                <IconButton size='xs' colorScheme={'blue'} icon={<DownloadIcon />} />
+                <IconButton
+                  size='xs'
+                  colorScheme={'blue'}
+                  icon={<DownloadIcon />}
+                />
               </Tooltip>
               <Tooltip label='SBOM Checks'>
-                <IconButton size='xs' colorScheme={ vulnRunStatus === 'FINISHED' || vulnRunStatus === 'IN_PROGRESS' ? 'blue' : 'blackAlpha' } icon={<Search2Icon />} />
+                <IconButton
+                  size='xs'
+                  colorScheme={
+                    vulnRunStatus === 'FINISHED' ||
+                    vulnRunStatus === 'IN_PROGRESS'
+                      ? 'blue'
+                      : 'blackAlpha'
+                  }
+                  icon={<Search2Icon />}
+                />
               </Tooltip>
               <Tooltip label='Vulnerability Scan'>
-                <IconButton size='xs' colorScheme={vulnRunStatus === 'FINISHED' ? 'blue' : 'blackAlpha'} icon={<FaBug />} />
+                <IconButton
+                  size='xs'
+                  colorScheme={
+                    vulnRunStatus === 'FINISHED' ? 'blue' : 'blackAlpha'
+                  }
+                  icon={<FaBug />}
+                />
               </Tooltip>
               <Tooltip label='Ready'>
-                <IconButton size='xs' colorScheme={vulnRunStatus === 'FINISHED' ? 'blue' : 'blackAlpha'} icon={<FaCircleCheck />} />
+                <IconButton
+                  size='xs'
+                  colorScheme={
+                    vulnRunStatus === 'FINISHED' ? 'blue' : 'blackAlpha'
+                  }
+                  icon={<FaCircleCheck />}
+                />
               </Tooltip>
               {vulnRunStatus === 'IN_PROGRESS' && (
-                <Badge px={2} py={1} fontWeight={'semibold'}>Scanning...</Badge>
+                <Badge px={2} py={1} fontWeight={'semibold'}>
+                  Scanning...
+                </Badge>
               )}
             </Flex>
           )}
           {/* UPDATED AT */}
           <Tooltip placement='top' label={getFullDateAndTime(updatedAt)}>
-            <Text width={'fit-content'} fontSize='xs' cursor={'pointer'}>Updated {timeSince(updatedAt)}</Text>
+            <Text width={'fit-content'} fontSize='xs' cursor={'pointer'}>
+              Updated {timeSince(updatedAt)}
+            </Text>
           </Tooltip>
           {/* STATS */}
           <Flex flexDir={'row'} alignItems={'center'} gap={8} mt={5}>
@@ -214,22 +345,54 @@ const SbomDetails = ({ sbom, getCompData, getVulnData }) => {
             <Stack direction={'row'} alignItems={'flex-start'} spacing={2}>
               <Icon h={4} w={4} mt={1} color='#777' as={FaCube} />
               <Flex flexDir={'column'} alignItems={'center'}>
-                <Tag size='md' variant='subtle' width={'full'} colorScheme={'blue'} cursor={'pointer'} onClick={onSelectComp} >
+                <Tag
+                  size='md'
+                  variant='subtle'
+                  width={'full'}
+                  colorScheme={'blue'}
+                  cursor={'pointer'}
+                  onClick={onSelectComp}
+                >
                   <TagLabel mx={'auto'}>{compCount}</TagLabel>
                 </Tag>
-                <Text mt={1} fontSize={'xs'} onClick={onSelectComp} cursor={'pointer'} _hover={{ textDecoration: 'underline' }}>
+                <Text
+                  mt={1}
+                  fontSize={'xs'}
+                  onClick={onSelectComp}
+                  cursor={'pointer'}
+                  _hover={{ textDecoration: 'underline' }}
+                >
                   Components
                 </Text>
               </Flex>
             </Stack>
             {/* LICENSES */}
             <Stack direction={'row'} alignItems={'flex-start'} spacing={2}>
-              <Icon mt={1} h={'20px'} w={'20px'} color='#777' as={FaBalanceScale} />
+              <Icon
+                mt={1}
+                h={'20px'}
+                w={'20px'}
+                color='#777'
+                as={FaBalanceScale}
+              />
               <Flex flexDir={'column'} alignItems={'center'}>
-                <Tag cursor={'pointer'} size='md' variant='subtle' width={16} colorScheme={'blue'} onClick={onSelectLicenses} >
+                <Tag
+                  cursor={'pointer'}
+                  size='md'
+                  variant='subtle'
+                  width={16}
+                  colorScheme={'blue'}
+                  onClick={onSelectLicenses}
+                >
                   <TagLabel mx={'auto'}>{compLicenseCount}</TagLabel>
                 </Tag>
-                <Text cursor={'pointer'} mt={1} fontSize={'xs'} _hover={{ textDecoration: 'underline' }} onClick={onSelectLicenses} >
+                <Text
+                  cursor={'pointer'}
+                  mt={1}
+                  fontSize={'xs'}
+                  _hover={{ textDecoration: 'underline' }}
+                  onClick={onSelectLicenses}
+                >
                   Licenses
                 </Text>
               </Flex>
@@ -239,24 +402,84 @@ const SbomDetails = ({ sbom, getCompData, getVulnData }) => {
               <Icon mt={1} h={4} w={4} color='#777' as={FaBug} />
               <Flex flexDir={'column'} alignItems={'center'}>
                 <Stack fontWeight={'medium'} direction={'row'}>
-                  <VulnBadge color='red' label='Critical' onClick={() => onFilterVuln(['critical'])} >
+                  <VulnBadge
+                    color='red'
+                    label='Critical'
+                    onClick={() => onFilterVuln(['critical'])}
+                  >
                     {critical ? critical : 0}
                   </VulnBadge>
-                  <VulnBadge color='orange' label='High' onClick={() => onFilterVuln(['high'])} >
+                  <VulnBadge
+                    color='orange'
+                    label='High'
+                    onClick={() => onFilterVuln(['high'])}
+                  >
                     {high ? high : 0}
                   </VulnBadge>
-                  <VulnBadge color='yellow' label='Medium' onClick={() => onFilterVuln(['medium'])} >
+                  <VulnBadge
+                    color='yellow'
+                    label='Medium'
+                    onClick={() => onFilterVuln(['medium'])}
+                  >
                     {medium ? medium : 0}
                   </VulnBadge>
-                  <VulnBadge color='green' label='Low' onClick={() => onFilterVuln(['low'])} >
+                  <VulnBadge
+                    color='green'
+                    label='Low'
+                    onClick={() => onFilterVuln(['low'])}
+                  >
                     {low ? low : 0}
                   </VulnBadge>
-                  <VulnBadge color='gray' label='Unknown' onClick={() => onFilterVuln(['unknown'])} >
+                  <VulnBadge
+                    color='gray'
+                    label='Unknown'
+                    onClick={() => onFilterVuln(['unknown'])}
+                  >
                     {unknown ? unknown : 0}
                   </VulnBadge>
                 </Stack>
-                <Text fontSize={'xs'} mt={1} onClick={() => onFilterVuln(null)} style={{ cursor: 'pointer' }} _hover={{ textDecoration: 'underline' }} >
+                <Text
+                  fontSize={'xs'}
+                  mt={1}
+                  onClick={() => onFilterVuln(null)}
+                  style={{ cursor: 'pointer' }}
+                  _hover={{ textDecoration: 'underline' }}
+                >
                   Vulnerabilities
+                </Text>
+              </Flex>
+            </Stack>
+            {/* POLICY RESULTS */}
+            <Stack direction={'row'} alignItems={'flex-start'} spacing={2}>
+              <Icon mt={1} h={5} w={5} color='#777' as={MdPolicy} />
+              <Flex flexDir={'column'} alignItems={'center'}>
+                <Stack fontWeight={'medium'} direction={'row'}>
+                  <VulnBadge color='red' label='FAIL'>
+                    {0}
+                  </VulnBadge>
+                  <VulnBadge color='yellow' label='WARN'>
+                    {0}
+                  </VulnBadge>
+                  <VulnBadge color='blue' label='INFORM'>
+                    {0}
+                  </VulnBadge>
+                  <VulnBadge color='green' label='PASS'>
+                    {0}
+                  </VulnBadge>
+                  <VulnBadge color='orange' label='SKIPPED'>
+                    {0}
+                  </VulnBadge>
+                  <VulnBadge color='gray' label='ERROR'>
+                    {0}
+                  </VulnBadge>
+                </Stack>
+                <Text
+                  fontSize={'xs'}
+                  mt={1}
+                  style={{ cursor: 'pointer' }}
+                  _hover={{ textDecoration: 'underline' }}
+                >
+                  Policy Results
                 </Text>
               </Flex>
             </Stack>
@@ -264,7 +487,18 @@ const SbomDetails = ({ sbom, getCompData, getVulnData }) => {
         </Flex>
       </Flex>
 
-      {isOpen && primaryComp && <GraphDrawer isOpen={isOpen} onClose={onClose} primaryComp={signedUrlParams ? primaryComp?.shareLynkQuery?.sbom?.components : primaryComp?.sbom?.components} activeComp={null} />}
+      {isOpen && primaryComp && (
+        <GraphDrawer
+          isOpen={isOpen}
+          onClose={onClose}
+          primaryComp={
+            signedUrlParams
+              ? primaryComp?.shareLynkQuery?.sbom?.components
+              : primaryComp?.sbom?.components
+          }
+          activeComp={null}
+        />
+      )}
     </>
   )
 }
