15c6f13d7db0959530b66673e67ebd623324f70c
Amal Jose Alex <howareyouami@gmail.com>
2024-05-30 17:53:35 +0530

    Test regex fixes (#3255)
    * Style updates
    
    * Fix margin

--- src/views/Dashboard/Profile/components/InternalComponents/TestRegex.js
+++ src/views/Dashboard/Profile/components/InternalComponents/TestRegex.js
@@ -1,8 +1,7 @@
 import { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
 
-import { DeleteIcon } from '@chakra-ui/icons'
-import { Box, Button, IconButton, Input, Text } from '@chakra-ui/react'
+import { Box, Button, Text, Textarea, Tooltip } from '@chakra-ui/react'
 
 import { FaCircleCheck } from 'react-icons/fa6'
 import { IoIosCloseCircle } from 'react-icons/io'
@@ -12,28 +11,16 @@ export const TextRegex = ({ regex }) => {
 
   return (
     <div>
-      <Text fontWeight='bold'>Testing:</Text>
+      <Text fontWeight='bold'>Test Regular Expression</Text>
       {items.map((item) => {
         const isMatch = new RegExp(regex).test(item.value)
         const color = isMatch ? 'green' : 'red'
         return (
           <Box display='flex' key={item.id} m={1}>
-            <Box
-              display='flex'
-              w={10}
-              justifyContent='center'
-              alignItems='center'
-            >
-              {isMatch ? (
-                <FaCircleCheck size={25} color={color} />
-              ) : (
-                <IoIosCloseCircle size={30} color={color} />
-              )}
-            </Box>
-            <Input
-              m={1}
+            <Textarea
+              placeholder='Enter a string to check if it will match the expression.'
+              my={1}
               borderColor={color}
-              borderWidth='medium'
               onChange={(e) => {
                 const newItems = items.map((oldItem) =>
                   oldItem.id === item.id
@@ -47,21 +34,26 @@ export const TextRegex = ({ regex }) => {
               }}
               value={item.value}
             />
-            <IconButton
-              mt={2}
-              icon={<DeleteIcon />}
-              colorScheme='red'
-              size='sm'
-              disabled={items.length === 1}
-              onClick={() => {
-                const newItems = items.filter(
-                  (oldItem) => oldItem.id !== item.id
-                )
-                setItems(newItems)
-              }}
+            <Tooltip
+              label={
+                isMatch
+                  ? 'Matches regular expression'
+                  : `Doesn't matches regular expression`
+              }
             >
-              Delete
-            </IconButton>
+              <Box
+                display='flex'
+                w={10}
+                justifyContent='center'
+                alignItems='center'
+              >
+                {isMatch ? (
+                  <FaCircleCheck size={25} color={color} />
+                ) : (
+                  <IoIosCloseCircle size={30} color={color} />
+                )}
+              </Box>
+            </Tooltip>
           </Box>
         )
       })}
