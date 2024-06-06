fb47e6264d3c622e0240e7077186570b8498af97
Amal Jose Alex <howareyouami@gmail.com>
2024-06-06 19:53:35 +0530

    Remove demo bypassing (#3298)
    * Fix typo
    
    * Cleanup

--- src/utils/shouldShowDemoFeatures.js
+++ src/utils/shouldShowDemoFeatures.js
@@ -3,9 +3,6 @@ const IS_DEMO_ENABLED = true
 const DEMO_ORG_NAME = 'Interlynk - Demo'
 
 export const shouldShowDemoFeatures = () => {
-  // temp setup
-  return true
-
   const orgName = localStorage.getItem('organization')
   return IS_DEMO_ENABLED && orgName === DEMO_ORG_NAME
 }
