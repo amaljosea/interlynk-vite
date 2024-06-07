02335b8741a1ae7bff07ad60ac5ed199c683aa10
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-06-07 15:01:07 +0530

    Update SBOM quality score bar with new changes (#3301)

--- src/views/Sbom/components/ScoresProgress.js
+++ src/views/Sbom/components/ScoresProgress.js
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { shouldShowDemoFeatures } from 'utils/shouldShowDemoFeatures'
 
-import { Box, Progress, Text, Tooltip } from '@chakra-ui/react'
+import { Box, Progress, Stack, Text } from '@chakra-ui/react'
 
 import { useQualityScore } from 'hooks/useQualityScore'
 
@@ -19,26 +19,56 @@ export const ScoresProgress = () => {
     skip: !showDemoFeatures
   })
 
+  const getColor = () => {
+    if (qualityScore < 30) {
+      return 'red'
+    } else if (qualityScore >= 30 && qualityScore <= 70) {
+      return 'blue'
+    } else {
+      return 'green'
+    }
+  }
+
   if (!shouldShowDemoFeatures()) {
     return null
   }
 
   return (
-    <Box>
-      <Tooltip
-        isDisabled={loading}
-        label={`${qualityScore}/100 or ${currentScore}/${maxScore}`}
-      >
-        <Box mb={5}>
-          <Text>SBOM quality score</Text>
-          <Progress value={qualityScore} size='lg' isIndeterminate={loading} />
+    <Stack spacing={0.4}>
+      <Box position='relative' display='inline-block' width='100%'>
+        <Progress
+          size='lg'
+          height={'1.5rem'}
+          value={qualityScore}
+          colorScheme={getColor()}
+          borderRadius={'0.375rem'}
+          isIndeterminate={loading}
+        />
+        <Box
+          position='absolute'
+          top='0'
+          left='0'
+          width='100%'
+          height='100%'
+          display='flex'
+          alignItems='center'
+          justifyContent='center'
+          color={qualityScore < 50 ? 'black' : 'white'}
+          fontWeight='medium'
+          fontSize={'xs'}
+        >
+          <Text>{qualityScore} / 100</Text>
         </Box>
-      </Tooltip>
-
-      {/* <Box mb={5}>
-        <Text>Health score</Text>
-        <Progress value={80} />
-      </Box> */}
-    </Box>
+      </Box>
+      <Text
+        pos={'relative'}
+        top={1}
+        textAlign={'center'}
+        fontSize={'xs'}
+        style={{ cursor: 'pointer' }}
+      >
+        SBOM Quality Score
+      </Text>
+    </Stack>
   )
 }
