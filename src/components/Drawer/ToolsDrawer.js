f3b444e8cf7989231a4cb4d6465acd5cc6348d36
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-06-28 19:35:17 +0530

    Update tools drawer with customer loader (#3512)

--- src/components/Drawer/ToolsDrawer.js
+++ src/components/Drawer/ToolsDrawer.js
@@ -11,6 +11,8 @@ import {
   Grid,
   GridItem,
   Heading,
+  SimpleGrid,
+  Skeleton,
   Stack,
   Tag,
   useColorModeValue
@@ -22,6 +24,18 @@ import DiffTable from 'components/Tables/DiffTable'
 
 import { useSbomCompare } from 'hooks/useSbomCompare'
 
+const Loader = () => {
+  return (
+    <Stack width={'100%'} dir='column' spacing={6}>
+      <SimpleGrid columns={2} spacing={6}>
+        <Skeleton width={'100%'} height={64} />
+        <Skeleton width={'100%'} height={64} />
+      </SimpleGrid>
+      <Skeleton width={'100%'} height={72} />
+    </Stack>
+  )
+}
+
 const ToolsDrawer = ({ sbomIdOne, sbomIdTwo, onClose }) => {
   const { sbomOne, sbomTwo, isLoading, diffs } = useSbomCompare({
     sbomIdOne,
@@ -32,10 +46,6 @@ const ToolsDrawer = ({ sbomIdOne, sbomIdTwo, onClose }) => {
   const sbomOneColor = useColorModeValue('green.100', 'gray.800')
   const sbomTwoColor = useColorModeValue('red.100', 'gray.800')
 
-  if (isLoading) {
-    return null
-  }
-
   return (
     <Drawer size='full' isOpen={true} placement='bottom' onClose={onClose}>
       <DrawerOverlay />
@@ -43,86 +53,96 @@ const ToolsDrawer = ({ sbomIdOne, sbomIdTwo, onClose }) => {
         <DrawerCloseButton mt={2} />
         <DrawerHeader>SBOM Comparison</DrawerHeader>
         <DrawerBody>
-          {/* SBOM INFO */}
-          <Grid templateColumns='repeat(2, 1fr)' gap={6} mb={12}>
-            {/* SBOM ONE */}
-            <GridItem w='100%'>
-              <Card
-                width='100%'
-                bg={sbomOneColor}
-                px={6}
-                h='450px'
-                overflowY='scroll'
-              >
-                {sbomOne && (
-                  <Flex width={'100%'} flexDirection={'column'}>
-                    {/* HEADIING */}
-                    <Stack>
-                      <Flex alignItems={'flex-end'} gap={1}>
-                        <Heading
-                          fontWeight={'semibold'}
-                          color={textColor}
-                          fontFamily={'inherit'}
-                          size='md'
-                        >
-                          {sbomOne?.project?.projectGroup?.name} :{' '}
-                          {sbomOne?.projectVersion}
-                        </Heading>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <Stack dir='column'>
+              {/* SBOM INFO */}
+              <Grid templateColumns='repeat(2, 1fr)' gap={6} mb={12}>
+                {/* SBOM ONE */}
+                <GridItem w='100%'>
+                  <Card
+                    width='100%'
+                    bg={sbomOneColor}
+                    px={6}
+                    h='450px'
+                    overflowY='scroll'
+                  >
+                    {sbomOne && (
+                      <Flex width={'100%'} flexDirection={'column'}>
+                        {/* HEADIING */}
+                        <Stack>
+                          <Flex alignItems={'flex-end'} gap={1}>
+                            <Heading
+                              fontWeight={'semibold'}
+                              color={textColor}
+                              fontFamily={'inherit'}
+                              size='md'
+                            >
+                              {sbomOne?.project?.projectGroup?.name} :{' '}
+                              {sbomOne?.projectVersion}
+                            </Heading>
+                          </Flex>
+                          <Tag
+                            variant='solid'
+                            colorScheme='green'
+                            width={'fit-content'}
+                            textTransform={'capitalize'}
+                          >
+                            {sbomOne?.project?.name}
+                          </Tag>
+                        </Stack>
+                        {/* DETAILS */}
+                        <SbomInfo data={sbomOne} />
                       </Flex>
-                      <Tag
-                        variant='solid'
-                        colorScheme='green'
-                        width={'fit-content'}
-                        textTransform={'capitalize'}
+                    )}
+                  </Card>
+                </GridItem>
+                {/* SBOM TWO */}
+                <GridItem w='100%'>
+                  <Card bg={sbomTwoColor} px={6} h='450px' overflowY='scroll'>
+                    {sbomTwo && (
+                      <Flex
+                        width='100%'
+                        flexWrap={'wrap'}
+                        flexDirection={'column'}
                       >
-                        {sbomOne?.project?.name}
-                      </Tag>
-                    </Stack>
-                    {/* DETAILS */}
-                    <SbomInfo data={sbomOne} />
-                  </Flex>
-                )}
-              </Card>
-            </GridItem>
-            {/* SBOM TWO */}
-            <GridItem w='100%'>
-              <Card bg={sbomTwoColor} px={6} h='450px' overflowY='scroll'>
-                {sbomTwo && (
-                  <Flex width='100%' flexWrap={'wrap'} flexDirection={'column'}>
-                    {/* HEADIING */}
-                    <Stack>
-                      <Heading
-                        fontWeight={'semibold'}
-                        color={textColor}
-                        fontFamily={'inherit'}
-                        size='md'
-                      >
-                        {sbomTwo?.project?.projectGroup?.name} :{' '}
-                        {sbomTwo?.projectVersion}
-                      </Heading>
-                      <Tag
-                        variant='solid'
-                        colorScheme='red'
-                        width={'fit-content'}
-                        textTransform={'capitalize'}
-                      >
-                        {sbomTwo?.project?.name}
-                      </Tag>
-                    </Stack>
-                    {/* DETAILS */}
-                    <SbomInfo data={sbomTwo} />
-                  </Flex>
-                )}
-              </Card>
-            </GridItem>
-          </Grid>
-          {/* SBOM DIFFERENCE */}
-          <DiffTable
-            diffs={diffs}
-            isLoading={isLoading}
-            sbomOne={sbomOne}
-            sbomTwo={sbomTwo}
-          />
+                        {/* HEADIING */}
+                        <Stack>
+                          <Heading
+                            fontWeight={'semibold'}
+                            color={textColor}
+                            fontFamily={'inherit'}
+                            size='md'
+                          >
+                            {sbomTwo?.project?.projectGroup?.name} :{' '}
+                            {sbomTwo?.projectVersion}
+                          </Heading>
+                          <Tag
+                            variant='solid'
+                            colorScheme='red'
+                            width={'fit-content'}
+                            textTransform={'capitalize'}
+                          >
+                            {sbomTwo?.project?.name}
+                          </Tag>
+                        </Stack>
+                        {/* DETAILS */}
+                        <SbomInfo data={sbomTwo} />
+                      </Flex>
+                    )}
+                  </Card>
+                </GridItem>
+              </Grid>
+              {/* SBOM DIFFERENCE */}
+              <DiffTable
+                diffs={diffs}
+                isLoading={isLoading}
+                sbomOne={sbomOne}
+                sbomTwo={sbomTwo}
+              />
+            </Stack>
+          )}
         </DrawerBody>
       </DrawerContent>
     </Drawer>
