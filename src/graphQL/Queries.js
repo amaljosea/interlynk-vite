16f2a41d8ae2a1972636386e4f1fa52be1726ef3
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-07-17 18:35:41 +0530

    Fixed version query for customer view (#3689)

--- src/graphQL/Queries.js
+++ src/graphQL/Queries.js
@@ -1250,6 +1250,7 @@ export const ShareVersionTable = gql`
           nodes {
             id
             creationAt
+            createdAt
             updatedAt
             lifecycle
             projectVersion
