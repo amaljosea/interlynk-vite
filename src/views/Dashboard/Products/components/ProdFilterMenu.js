fa51f68f62bee5ef47086bad03b442f654051d1e
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-07-24 19:14:32 +0530

    Add label filter in products table (#3771)

--- src/views/Dashboard/Products/components/ProdFilterMenu.js
+++ src/views/Dashboard/Products/components/ProdFilterMenu.js
@@ -1,10 +1,20 @@
+import { useState } from 'react'
+
 import { Box, Menu, Stack } from '@chakra-ui/react'
 
 import CheckMark from 'components/Misc/CheckMark'
 import CustomList from 'components/Misc/CustomList'
 import MenuHeading from 'components/Misc/MenuHeading'
 
+import { useShouldShowDemoFeatures } from 'hooks/useShouldShowDemoFeatures'
+
 const ProdFilterMenu = ({ enabled, onFilter }) => {
+  const { shouldShowDemoFeatures } = useShouldShowDemoFeatures()
+  const [labels, setLabels] = useState([])
+  const onFilterLabel = (value) => {
+    setLabels(value?.includes('all') ? [] : value)
+  }
+
   return (
     <Stack direction={'row'} alignItems={'center'} gap={1}>
       {/* ACTIVE */}
@@ -17,6 +27,30 @@ const ProdFilterMenu = ({ enabled, onFilter }) => {
             options={['yes', 'no']}
             value={enabled === true ? 'yes' : enabled === false ? 'no' : 'all'}
             onChange={onFilter}
+          />
+        </Menu>
+      </Box>
+      {/* LABELS */}
+      <Box
+        width={'fit-content'}
+        position={'relative'}
+        display={shouldShowDemoFeatures ? 'flex' : 'none'}
+      >
+        <Menu closeOnSelect={false}>
+          {labels?.length !== 0 && <CheckMark />}
+          <MenuHeading title={'Labels'} />
+          <CustomList
+            type='checkbox'
+            options={[
+              'bug',
+              'demo',
+              'dependencies',
+              'documentation',
+              'enhancement',
+              'FDA'
+            ]}
+            value={labels}
+            onChange={onFilterLabel}
           />
         </Menu>
       </Box>
