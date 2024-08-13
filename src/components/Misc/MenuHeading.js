88408fbd0de8cc223c4018afd3633110bbea96da
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-08-13 12:48:10 +0530

    Update filter component (#3938)

--- src/components/Misc/MenuHeading.js
+++ src/components/Misc/MenuHeading.js
@@ -4,7 +4,7 @@ import { FaFilter } from 'react-icons/fa'
 
 const MenuHeading = ({ title, onClick, active }) => {
   const grayBorder = useColorModeValue('#1A202C29', '#ffffff29')
-  const grayText = useColorModeValue('#03030399', '#E2E8F0')
+  const grayText = useColorModeValue('#1A202C', '#f1f1f1')
   const bgActive = useColorModeValue('#EDF2F7', '')
   const iconColor = useColorModeValue('#3182CE', '#90cdf4')
   const activeBorderColor = useColorModeValue('#3182CE', '#90cdf499')
