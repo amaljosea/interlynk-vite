91bebccadbdb4fa68439f89e1bf0f87034a88234
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-08-19 16:03:14 +0530

    ✅ Reorganized compliance checks (#3995)
    * Reorganized compliance checks
    
    * Remove unused variables

---
+++ src/views/Sbom/components/ComplianceChecks.js
@@ -0,0 +1,188 @@
+import { useState } from 'react'
+
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Tag,
+  Text,
+  useColorModeValue
+} from '@chakra-ui/react'
+
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa'
+
+const ComplianceChecks = ({
+  name,
+  isOpen,
+  onClose,
+  ntia,
+  fda,
+  ntiaLoading,
+  fdaLoading
+}) => {
+  const bgColor = useColorModeValue('white', 'gray.700')
+  const tabs = ['NTIA', 'FDA 510(K)', 'BSI TR-03183']
+  const [tab, setTab] = useState(0)
+  const onTabChange = (value) => setTab(value)
+
+  const sbomCategory = ['Timestamp', 'Supplier Name', 'Unique ID', 'Author']
+
+  const getBgColor = (score) => {
+    if (score === 0) {
+      return '#FED7D7'
+    } else if (score === 100) {
+      return '#C6F6D5'
+    } else {
+      return '#FEEBC8'
+    }
+  }
+
+  const ComplianceReport = ({ key, item, loading }) => {
+    return (
+      <Flex
+        gap={6}
+        key={key}
+        width={'100%'}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+      >
+        <Text fontSize={'xs'} textTransform={'capitalize'}>
+          {item?.category?.replace('Component ', '')}
+        </Text>
+        <Box fontSize={'xs'} ml={'auto'}>
+          {sbomCategory?.includes(item?.category) ? (
+            <>
+              {item?.score === 100 ? (
+                <FaCheckCircle color='green' size={20} />
+              ) : (
+                <FaTimesCircle color='red' size={20} />
+              )}
+            </>
+          ) : (
+            <Button
+              size='xs'
+              color={'#000'}
+              width={'80px'}
+              isLoading={loading}
+              bg={getBgColor(item?.score)}
+              _hover={{ background: getBgColor(item?.score) }}
+            >
+              <span> {Math.round(item?.score)} %</span>
+            </Button>
+          )}
+        </Box>
+      </Flex>
+    )
+  }
+
+  const ScoreBoard = ({ data, loading }) => {
+    const sbomData = data?.scoreByCategory?.filter(
+      (item) => !item?.category?.startsWith('Component')
+    )
+    const compData = data?.scoreByCategory?.filter((item) =>
+      item?.category?.startsWith('Component')
+    )
+
+    return (
+      <Flex flexDir={'column'} alignItems={'flex-start'} gap={3}>
+        <Text fontSize={'sm'} fontWeight={'medium'}>
+          Details
+        </Text>
+        {sbomData?.map((item, index) => (
+          <ComplianceReport key={index} item={item} loading={loading} />
+        ))}
+        <Text mt={3} fontSize={'sm'} fontWeight={'medium'}>
+          Component Details
+        </Text>
+        {compData?.map((item, index) => (
+          <ComplianceReport key={index} item={item} loading={loading} />
+        ))}
+        <Divider mt={1} />
+        <Flex
+          mt={1}
+          width={'100%'}
+          alignItems={'center'}
+          justifyContent={'space-between'}
+        >
+          <Text fontSize={'sm'} fontWeight={'medium'}>
+            Score
+          </Text>
+          <Button size='xs'>{Math.round(data?.score)} %</Button>
+        </Flex>
+      </Flex>
+    )
+  }
+
+  return (
+    <Drawer size='sm' isOpen={isOpen} placement='right' onClose={onClose}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerCloseButton mt={2} />
+        <DrawerHeader pl={4}>
+          <Text mb={1}>Compliance Checks</Text>
+          <Tag
+            p={2}
+            fontSize={'xs'}
+            w={'fit-content'}
+            colorScheme='blue'
+            textAlign={'right'}
+            wordBreak={'break-all'}
+          >
+            {name}
+          </Tag>
+        </DrawerHeader>
+        <Divider />
+        <DrawerBody p={0}>
+          <Tabs index={tab} onChange={onTabChange}>
+            <TabList
+              position={'fixed'}
+              bg={bgColor}
+              zIndex={1}
+              left={0}
+              right={0}
+            >
+              {tabs.map((item, index) => (
+                <Tab
+                  py={3.5}
+                  key={index}
+                  fontSize={'sm'}
+                  textTransform={'capitalize'}
+                  _focus={{ outline: 'none', bg: 'none' }}
+                >
+                  {item}
+                </Tab>
+              ))}
+            </TabList>
+            <TabPanels pos={'relative'} top={14} overflowX={'hidden'}>
+              <TabPanel>
+                <ScoreBoard loading={ntiaLoading} data={ntia} />
+              </TabPanel>
+              <TabPanel>
+                <ScoreBoard loading={fdaLoading} data={fda} />
+              </TabPanel>
+              <TabPanel>
+                <Flex alignItems={'center'} justifyContent={'center'}>
+                  <Text py={24} color={'gray.500'}>Coming Soon...</Text>
+                </Flex>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </DrawerBody>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+export default ComplianceChecks
--- src/views/Sbom/components/DownloadModal.js
+++ src/views/Sbom/components/DownloadModal.js
@@ -1,20 +1,15 @@
 import { useLazyQuery, useQuery } from '@apollo/client'
 import { useState } from 'react'
 
+import { DownloadIcon } from '@chakra-ui/icons'
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
   Button,
   Checkbox,
   Divider,
   Flex,
   FormControl,
   FormLabel,
-  Grid,
-  GridItem,
+  Icon,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -26,7 +21,9 @@ import {
   RadioGroup,
   Stack,
   Tag,
-  Text
+  Text,
+  useColorModeValue,
+  useDisclosure
 } from '@chakra-ui/react'
 
 import useCustomToast from 'hooks/useCustomToast'
@@ -37,7 +34,7 @@ import {
   SignedSbomDownload
 } from 'graphQL/Queries'
 
-import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa'
+import ComplianceChecks from './ComplianceChecks'
 
 const DownloadModal = ({
   finalRef,
@@ -56,6 +53,11 @@ const DownloadModal = ({
     signedUrlParams ? SignedSbomDownload : DownloadSBOM
   )
 
+  const [spec, setSpec] = useState('cyclonedx')
+  const [format, setFormat] = useState('json')
+  const [includeVulns, setIncludeVulns] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
   const { data: ntia, loading: ntiaLoading } = useQuery(GetSbomQualityScores, {
     skip: isOpen && !signedUrlParams ? false : true,
     variables: { sbomIds: [sbomId], reportFormat: 'NTIA' }
@@ -68,10 +70,13 @@ const DownloadModal = ({
   const { nodes: ntiaData } = ntia?.complianceReports || ''
   const { nodes: fdaData } = fda?.complianceReports || ''
 
-  const [spec, setSpec] = useState('cyclonedx')
-  const [format, setFormat] = useState('json')
-  const [includeVulns, setIncludeVulns] = useState(false)
-  const [isLoading, setIsLoading] = useState(false)
+  const {
+    isOpen: isOpenDetails,
+    onOpen: onOpenDetails,
+    onClose: onCloseDetails
+  } = useDisclosure()
+
+  const textColor = useColorModeValue('#1A202C', '#f6f6f6')
 
   const type = spec === 'cyclonedx' ? 'cdx' : 'spdx'
 
@@ -136,204 +141,174 @@ const DownloadModal = ({
     }
   }
 
-  const sbomCategory = ['Timestamp', 'Supplier Name', 'Unique ID', 'Author']
-
-  const getBgColor = (score) => {
-    if (score === 0) {
-      return '#FED7D7'
-    } else if (score === 100) {
-      return '#C6F6D5'
-    } else {
-      return '#FEEBC8'
-    }
-  }
-
-  const ScoreBoard = ({ data, loading }) => {
-    return (
-      <>
-        {data?.scoreByCategory?.map((item, index) => (
-          <Grid key={index} templateColumns='repeat(2, 1fr)' gap={6} mb={3}>
-            <GridItem fontSize={'xs'} textTransform={'capitalize'}>
-              {item?.category}
-            </GridItem>
-            <GridItem fontSize={'xs'} ml={'auto'}>
-              <Button
-                size='xs'
-                width={'80px'}
-                isLoading={loading}
-                color={'#000'}
-                bg={getBgColor(item?.score)}
-                _hover={{ background: getBgColor(item?.score) }}
-              >
-                {sbomCategory?.includes(item?.category) ? (
-                  <>
-                    {item?.score === 100 ? (
-                      <FaCheckCircle color='green' size={16} />
-                    ) : (
-                      <FaTimesCircle color='red' size={16} />
-                    )}
-                  </>
-                ) : (
-                  <span> {Math.round(item?.score)} %</span>
-                )}
-              </Button>
-            </GridItem>
-          </Grid>
-        ))}
-      </>
-    )
-  }
-
   const checklists = [
     {
       name: 'NTIA Minimum Elements',
       loading: ntiaLoading,
-      score: ntiaData?.length > 0 ? Math.round(ntiaData[0].score) : 0,
-      data: (
-        <ScoreBoard
-          loading={ntiaLoading}
-          data={ntiaData?.length > 0 ? ntiaData[0] : []}
-        />
-      )
+      score: ntiaData?.length > 0 ? Math.round(ntiaData[0].score) : 0
     },
     {
       name: 'FDA 510(K) Compliance',
       loading: fdaLoading,
-      score: fdaData?.length > 0 ? Math.round(fdaData[0].score) : 0,
-      data: (
-        <ScoreBoard
-          loading={fdaLoading}
-          data={fdaData?.length > 0 ? fdaData[0] : []}
-        />
-      )
+      score: fdaData?.length > 0 ? Math.round(fdaData[0].score) : 0
     },
     {
       name: 'BSI TR-03183',
-      score: '',
-      data: <Text>Coming soon...</Text>
+      loading: false,
+      score: 0
     }
   ]
 
+  const fileName = `${productName}-${version}.${type}.xml`
+
   return (
-    <Modal
-      size='lg'
-      isOpen={isOpen}
-      onClose={onClose}
-      finalFocusRef={finalRef}
-      initialFocusRef={initialRef}
-    >
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>Download SBOM</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody pb={6}>
-          <Flex flexDirection={'column'} gap={4}>
-            <FormControl>
-              <FormLabel>Specification</FormLabel>
-              <RadioGroup value={spec} onChange={(value) => setSpec(value)}>
-                <Stack spacing={4} direction='row'>
-                  {(activeUser === 'sp@interlynk.io' ||
-                    activeUser === 'surendra.pathak@interlynk') && (
-                    <Radio value='spdx'>SPDX</Radio>
-                  )}
-                  <Radio value='cyclonedx'>CycloneDX</Radio>
-                </Stack>
-              </RadioGroup>
-            </FormControl>
-            <FormControl>
-              <FormLabel>File Format</FormLabel>
-              <RadioGroup value={format} onChange={(value) => setFormat(value)}>
-                <Stack spacing={4} direction='row'>
-                  <Radio value='json'>JSON</Radio>
-                  {/* <Radio value='xml'>XML</Radio> */}
-                </Stack>
-              </RadioGroup>
-            </FormControl>
-            <Checkbox
-              isChecked={includeVulns}
-              onChange={() => setIncludeVulns(!includeVulns)}
-              isDisabled={spec === 'spdx'}
-            >
-              Include Vulnerabilities
-            </Checkbox>
-            <Divider />
-            <Flex
-              gap={2}
-              flexWrap={'wrap'}
-              alignItems={'center'}
-              justifyContent={'space-between'}
-            >
-              <Text fontWeight={'medium'} fontSize={'sm'} width='100px'>
-                File Name
-              </Text>
+    <>
+      <Modal
+        size='lg'
+        isOpen={isOpen}
+        onClose={onClose}
+        finalFocusRef={finalRef}
+        initialFocusRef={initialRef}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader as={Flex} alignItems='center' gap={2.5}>
+            <Icon as={DownloadIcon} color='#60686F' />
+            <Text fontWeight={600}>Download SBOM</Text>
+          </ModalHeader>
+          <ModalCloseButton mt={2} />
+          <Divider />
+          <ModalBody pb={6} mt={2.5}>
+            <Flex flexDirection={'column'} gap={4}>
               <Tag
                 p={2}
                 fontSize={'xs'}
+                w={'fit-content'}
                 colorScheme='blue'
                 textAlign={'right'}
                 wordBreak={'break-all'}
-              >{`${productName}-${version}.${type}.xml`}</Tag>
-            </Flex>
-            <FormControl display={!signedUrlParams ? 'block' : 'none'}>
-              <FormLabel>Compliance Checks</FormLabel>
-              <Accordion mt={3} allowMultiple>
-                {checklists.map((item, index) => (
-                  <AccordionItem key={index}>
-                    <AccordionButton
-                      _focus={{ outline: 'none' }}
-                      _expanded={{
-                        bg: 'blue.100',
-                        color: '#111',
-                        boxShadow: 'none',
-                        outline: 'none'
-                      }}
+              >
+                {fileName}
+              </Tag>
+              <FormControl>
+                <FormLabel>Specification</FormLabel>
+                <RadioGroup value={spec} onChange={(value) => setSpec(value)}>
+                  <Stack spacing={4} direction='row'>
+                    {(activeUser === 'sp@interlynk.io' ||
+                      activeUser === 'surendra.pathak@interlynk') && (
+                      <Radio value='spdx'>SPDX</Radio>
+                    )}
+                    <Radio value='cyclonedx'>CycloneDX</Radio>
+                  </Stack>
+                </RadioGroup>
+              </FormControl>
+              <FormControl>
+                <FormLabel>File Format</FormLabel>
+                <RadioGroup
+                  value={format}
+                  onChange={(value) => setFormat(value)}
+                >
+                  <Stack spacing={4} direction='row'>
+                    <Radio value='json'>JSON</Radio>
+                    {/* <Radio value='xml'>XML</Radio> */}
+                  </Stack>
+                </RadioGroup>
+              </FormControl>
+              <Checkbox
+                isChecked={includeVulns}
+                onChange={() => setIncludeVulns(!includeVulns)}
+                isDisabled={spec === 'spdx'}
+              >
+                Include Vulnerabilities
+              </Checkbox>
+              <Divider />
+              <Flex
+                flexDir={'column'}
+                gap={4}
+                display={!signedUrlParams ? 'flex' : 'none'}
+              >
+                <Flex
+                  flexDir={'row'}
+                  alignItems='center'
+                  justifyContent='space-between'
+                >
+                  <Text fontWeight={'medium'}>Compliance Checks</Text>
+                  <Text
+                    color='blue.500'
+                    cursor={'pointer'}
+                    fontWeight={'medium'}
+                    onClick={onOpenDetails}
+                  >
+                    View Details
+                  </Text>
+                </Flex>
+                <Flex gap={4} flexDir={'column'} alignItems='flex-start'>
+                  {checklists.map((item, index) => (
+                    <Flex
+                      w={'100%'}
+                      key={index}
+                      alignItems='center'
+                      justifyContent={'space-between'}
                     >
-                      <Flex
-                        width={'100%'}
-                        alignItems={'center'}
-                        justifyContent={'space-between'}
+                      <Text fontSize={'sm'} color={textColor}>
+                        {item?.name}
+                      </Text>
+                      <Button
+                        size='xs'
+                        isLoading={item?.loading}
+                        isDisabled={item?.score === 0}
                       >
-                        <Text fontSize={'sm'}>{item?.name}</Text>
-                        <Button
-                          size='xs'
-                          ml={'auto'}
-                          isLoading={item?.loading}
-                          hidden={item?.score === ''}
-                        >
-                          {item?.score} %
-                        </Button>
-                      </Flex>
-                      <AccordionIcon ml={2} />
-                    </AccordionButton>
-                    <AccordionPanel pb={4}>{item?.data}</AccordionPanel>
-                  </AccordionItem>
-                ))}
-              </Accordion>
-            </FormControl>
-          </Flex>
-        </ModalBody>
-        <ModalFooter>
-          <Flex
-            width={'100%'}
-            alignItems={'center'}
-            justifyContent={'flex-end'}
-            gap={4}
-          >
-            <Button colorScheme='gray' onClick={onClose}>
-              Cancel
-            </Button>
-            <Button
-              colorScheme='blue'
-              onClick={handleDownload}
-              isLoading={isLoading}
-              loadingText='Loading...'
+                        {item?.score === 0
+                          ? `Coming Soon..`
+                          : `${item?.score} %`}
+                      </Button>
+                    </Flex>
+                  ))}
+                </Flex>
+              </Flex>
+            </Flex>
+          </ModalBody>
+          <Divider />
+          <ModalFooter>
+            <Flex
+              gap={4}
+              width={'100%'}
+              alignItems={'center'}
+              justifyContent={'flex-end'}
             >
-              Download
-            </Button>
-          </Flex>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+              <Button
+                variant='ghost'
+                color={textColor}
+                onClick={onClose}
+                fontWeight={'normal'}
+              >
+                Cancel
+              </Button>
+              <Button
+                colorScheme='blue'
+                isLoading={isLoading}
+                loadingText='Loading...'
+                onClick={handleDownload}
+              >
+                Download
+              </Button>
+            </Flex>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+
+      {isOpenDetails && (
+        <ComplianceChecks
+          name={fileName}
+          isOpen={isOpenDetails}
+          fdaLoading={fdaLoading}
+          onClose={onCloseDetails}
+          ntiaLoading={ntiaLoading}
+          fda={fdaData?.length > 0 ? fdaData[0] : []}
+          ntia={ntiaData?.length > 0 ? ntiaData[0] : []}
+        />
+      )}
+    </>
   )
 }
 
