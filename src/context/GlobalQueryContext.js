9c5553394b734c635ccb331c050e1d9aaecee0ec
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-08-22 12:35:00 +0530

    Fixed global context (#4032)

--- src/context/GlobalQueryContext.js
+++ src/context/GlobalQueryContext.js
@@ -1,4 +1,5 @@
 import { useQuery } from '@apollo/client'
+import Cookies from 'js-cookie'
 import { createContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
@@ -15,6 +16,7 @@ export const GlobalQueryContext = createContext({
 
 export const GlobalQueryContextWrapper = ({ children }) => {
   const params = useParams()
+  const authToken = Cookies.get('authToken')
   const sbomHookData = useSbom({
     projectId: params?.productid,
     sbomId: params?.sbomid,
@@ -25,7 +27,7 @@ export const GlobalQueryContextWrapper = ({ children }) => {
   const [isFreeTier, setIsFreeTier] = useState(false)
 
   const { data: orgData, loading: orgQueryLoading } = useQuery(GetOrgName, {
-    fetchPolicy: 'network-only'
+    skip: authToken ? false : true
   })
 
   useEffect(() => {
