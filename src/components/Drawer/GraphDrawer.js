191e719838c68555f0e96dac277d61e7da28c91b
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-09-16 12:02:08 +0530

    ✅ Add expand action to the component relation drawer (#4364)
    * Add expand action to the component relation drawer
    
    * Update code

--- src/components/Drawer/GraphDrawer.js
+++ src/components/Drawer/GraphDrawer.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import GraphView from 'views/Sbom/components/GraphView'
 
 import {
@@ -7,17 +8,24 @@ import {
   DrawerContent,
   DrawerHeader,
   DrawerOverlay,
+  IconButton,
   Text
 } from '@chakra-ui/react'
 
 import CompInfo from 'components/Misc/CompInfo'
 
+import { FaExpand } from 'react-icons/fa6'
+
 const GraphDrawer = ({ isOpen, onClose, primaryComp, activeComp }) => {
+  const [drawerSize, setDrawerSize] = useState('lg')
+  const onExpand = () => setDrawerSize((prev) => (prev === 'lg' ? '2xl' : 'lg'))
+  const expandStyle = { top: 4, right: 14, pos: 'absolute' }
   return (
-    <Drawer isOpen={isOpen} placement='right' size='lg' onClose={onClose}>
+    <Drawer isOpen={isOpen} placement='right' size={drawerSize} onClose={onClose}>
       <DrawerOverlay />
       <DrawerContent>
-        <DrawerCloseButton mt={3} />
+        <DrawerCloseButton mt={2} />
+        <IconButton size='sm' sx={expandStyle} icon={<FaExpand />} onClick={onExpand} />
         <DrawerHeader borderBottomWidth='1px'>
           <Text fontWeight={'medium'}>Relationships</Text>
           {activeComp && <CompInfo data={activeComp} />}
