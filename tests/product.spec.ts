b5e60af739248d14a00bfacb89939acf6e546d4f
Amal Jose Alex <howareyouami@gmail.com>
2024-09-23 20:22:10 +0530

    updated tests and section scripts (#4436)

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -22,10 +22,8 @@ test('TC_004 Products CRUD Operation Test', async ({ page }) => {
   await pp.productsCrudFunctionality()
 })
 
-const isCI = process.env.CI === 'true'
-
-test('TC_007 Products And Version Switching Test', async ({ page }) => {
-  test.setTimeout(isCI ? 320000 : 240000)
+test('TC_008 Products And Version Switching Test', async ({ page }) => {
+  test.setTimeout(320000)
   const lp = new LoginPage(page)
   await lp.appLoginCommonFunctionality(email, password)
   const pp = new ProductPage(page)
