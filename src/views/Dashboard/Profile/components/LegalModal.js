071ad7555491efea82b012aa40ae96c21108a331
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-09-25 07:40:54 +0530

    Update manufacturer modal styling (#4447)

--- src/views/Dashboard/Profile/components/LegalModal.js
+++ src/views/Dashboard/Profile/components/LegalModal.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react'
 import { validateEmail } from 'utils'
 
 import {
-  Box,
   Button,
   Flex,
   FormControl,
@@ -36,23 +35,21 @@ const validateUrl = (url) => {
 }
 
 const LegalModal = ({ data, isOpen, onClose }) => {
-  const [orgName, setOrgName] = useState('')
   const [url, setUrl] = useState('')
-  // {
-  //   id: 1,
-  //   name: '',
-  //   email: '',
-  //   phone: '',
-  //   status: 'CREATED',
-  //   emailError: '',
-  //   phError: ''
-  // }
-  const [contacts, setContacts] = useState([])
-  const [deletedContacts, setDeletedContacts] = useState([])
   const [error, setError] = useState('')
+  const [orgName, setOrgName] = useState('')
+  const [contacts, setContacts] = useState([])
   const [isValidUrl, setIsValidUrl] = useState('')
+  const [deletedContacts, setDeletedContacts] = useState([])
 
   const borderColor = useColorModeValue('gray.200', 'gray.600')
+
+  const [createMfc, { loading: crLoading }] = useMutation(
+    OrganizationManufacturerCreate
+  )
+  const [updateMfc, { loading: upLoading }] = useMutation(
+    OrganizationManufacturerUpdate
+  )
 
   const containsSpace = /\s/.test(url)
   const checkDataValidity = (data) => {
@@ -68,7 +65,7 @@ const LegalModal = ({ data, isOpen, onClose }) => {
     return null
   }
 
-  const errorMessage = checkDataValidity(contacts)
+  const errorMessage = checkDataValidity(contacts) || crLoading || upLoading
 
   const checkEmptyValues = (data) => {
     for (let i = 0; i < data.length; i++) {
@@ -82,11 +79,7 @@ const LegalModal = ({ data, isOpen, onClose }) => {
 
   const emptyRow = checkEmptyValues(contacts)
 
-  const [createMfc] = useMutation(OrganizationManufacturerCreate)
-  const [updateMfc] = useMutation(OrganizationManufacturerUpdate)
-
-  const handleCreate = (e) => {
-    e.preventDefault()
+  const handleCreate = () => {
     if (url !== '' && !validateUrl(url)) {
       setIsValidUrl('Please enter a valid URL including http:// or https://')
     } else {
@@ -114,8 +107,7 @@ const LegalModal = ({ data, isOpen, onClose }) => {
     }
   }
 
-  const handleUpdate = (e) => {
-    e.preventDefault()
+  const handleUpdate = () => {
     const removedData = deletedContacts?.map((item) => ({
       id: item?.status === 'ADDED' ? item?.id : undefined,
       name: item?.name,
@@ -270,152 +262,140 @@ const LegalModal = ({ data, isOpen, onClose }) => {
           emailError: '',
           phError: ''
         }))
-        console.log('result', result)
         setContacts(result)
       }
     }
   }, [data])
 
   return (
-    <>
-      <LynkModal
-        isOpen={isOpen}
-        onClose={onClose}
-        onSubmit={data ? handleUpdate : handleCreate}
-        title={`${data ? 'Edit' : 'Add'} Manufacturer`}
-        Icon={BsGear}
-        disabled={
-          errorMessage || orgName === '' || (url !== '' && isValidUrl !== '')
-        }
-        buttonText={data ? 'Update' : 'Save'}
-      >
-        <Flex width={'100%'} direction={'column'} gap={4}>
-          <Box>
-            <FormControl isRequired>
-              <FormLabel fontSize={12}>Organization Name</FormLabel>
-              <Input
-                type='text'
-                value={orgName}
-                onChange={(e) => {
-                  setOrgName(e.target.value)
-                  setError('')
-                }}
-              />
-            </FormControl>
-          </Box>
-          <Box>
-            <FormControl isInvalid={isValidUrl !== '' || containsSpace}>
-              <FormLabel fontSize={12}>URL</FormLabel>
-              <Input
-                type='text'
-                value={url}
-                onChange={onChangeUrl}
-                placeholder='Add URL'
-                fontSize={14}
-              />
-              <FormErrorMessage>{isValidUrl}</FormErrorMessage>
-            </FormControl>
-          </Box>
-          <Flex direction='column' gap={2}>
-            <Flex
-              width={'100%'}
-              my={2}
-              justifyContent={'space-between'}
-              alignItems={'center'}
-            >
-              <Stack spacing={0} alignItems={'flex-start'}>
-                <Heading fontWeight={500} fontFamily={'inherit'} fontSize={12}>
-                  Contacts
-                </Heading>
-              </Stack>
-            </Flex>
-            {contacts?.length > 0 &&
-              contacts?.map((item, index) => (
-                <Flex key={index} alignItems={'flex-start'} gap={2}>
-                  <FormControl>
-                    <Input
-                      type='text'
-                      maxLength={50}
-                      placeholder='Name'
-                      value={item?.name}
-                      onChange={(e) =>
-                        handleChange(e.target.value, item.id, 'name')
-                      }
-                      fontSize={12}
-                    />
-                  </FormControl>
-                  <FormControl
-                    isInvalid={item?.email !== '' && item?.emailError !== ''}
-                  >
-                    <Input
-                      type='email'
-                      placeholder='Email'
-                      value={item?.email}
-                      onBlur={(e) => onEmailBlur(e, item)}
-                      onChange={(e) =>
-                        handleChange(e.target.value, item.id, 'email')
-                      }
-                      fontSize={12}
-                    />
-                    {item?.email !== '' && item?.emailError !== '' && (
-                      <FormErrorMessage fontSize='xs'>
-                        {item?.emailError}
-                      </FormErrorMessage>
-                    )}
-                  </FormControl>
-                  <FormControl
-                    isInvalid={item?.phone !== '' && item?.phError !== ''}
-                  >
-                    <Input
-                      type='text'
-                      placeholder='Phone number'
-                      value={item?.phone}
-                      onBlur={(e) => onPhoneBlur(e, item)}
-                      onChange={(e) =>
-                        handleChange(e.target.value, item.id, 'phone')
-                      }
-                      fontSize={12}
-                    />
-                    {item?.phone !== '' && item?.phError !== '' && (
-                      <FormErrorMessage fontSize='xs'>
-                        {item?.phError}
-                      </FormErrorMessage>
-                    )}
-                  </FormControl>
-                  <IconButton
-                    border='1px solid'
-                    colorScheme='white'
-                    borderColor={borderColor}
-                    aria-label='Remove config'
-                    onClick={() => deleteRow(item)}
-                    icon={
-                      <Icon
-                        color={'#E53E3E'}
-                        w={6}
-                        h={6}
-                        as={MdDeleteOutline}
-                      />
-                    }
-                  />
-                </Flex>
-              ))}
+    <LynkModal
+      Icon={BsGear}
+      isOpen={isOpen}
+      onClose={onClose}
+      buttonText={data ? 'Update' : 'Save'}
+      onSubmit={data ? handleUpdate : handleCreate}
+      title={`${data ? 'Edit' : 'Add'} Manufacturer`}
+      disabled={errorMessage || (url !== '' && isValidUrl !== '')}
+    >
+      <Flex width={'100%'} direction={'column'} gap={4}>
+        <FormControl isRequired>
+          <FormLabel fontSize={12}>Organization Name</FormLabel>
+          <Input
+            type='text'
+            value={orgName}
+            onChange={(e) => {
+              setOrgName(e.target.value)
+              setError('')
+            }}
+            placeholder='Add organization name'
+          />
+        </FormControl>
+        <FormControl isInvalid={isValidUrl !== '' || containsSpace}>
+          <FormLabel fontSize={12}>URL</FormLabel>
+          <Input
+            type='text'
+            value={url}
+            onChange={onChangeUrl}
+            placeholder='Add URL'
+            fontSize={14}
+          />
+          <FormErrorMessage>{isValidUrl}</FormErrorMessage>
+        </FormControl>
+        <Flex direction='column' gap={2}>
+          <Flex
+            width={'100%'}
+            my={2}
+            justifyContent={'space-between'}
+            alignItems={'center'}
+          >
+            <Stack spacing={0} alignItems={'flex-start'}>
+              <Heading fontWeight={500} fontFamily={'inherit'} fontSize={12}>
+                Contacts
+              </Heading>
+            </Stack>
           </Flex>
+          {contacts?.length > 0 &&
+            contacts?.map((item, index) => (
+              <Flex key={index} alignItems={'flex-start'} gap={2}>
+                <FormControl>
+                  <Input
+                    type='text'
+                    maxLength={50}
+                    placeholder='Name'
+                    value={item?.name}
+                    onChange={(e) =>
+                      handleChange(e.target.value, item.id, 'name')
+                    }
+                    fontSize={12}
+                  />
+                </FormControl>
+                <FormControl
+                  isInvalid={item?.email !== '' && item?.emailError !== ''}
+                >
+                  <Input
+                    type='email'
+                    placeholder='Email'
+                    value={item?.email}
+                    onBlur={(e) => onEmailBlur(e, item)}
+                    onChange={(e) =>
+                      handleChange(e.target.value, item.id, 'email')
+                    }
+                    fontSize={12}
+                  />
+                  {item?.email !== '' && item?.emailError !== '' && (
+                    <FormErrorMessage fontSize='xs'>
+                      {item?.emailError}
+                    </FormErrorMessage>
+                  )}
+                </FormControl>
+                <FormControl
+                  isInvalid={item?.phone !== '' && item?.phError !== ''}
+                >
+                  <Input
+                    type='text'
+                    placeholder='Phone number'
+                    value={item?.phone}
+                    onBlur={(e) => onPhoneBlur(e, item)}
+                    onChange={(e) =>
+                      handleChange(e.target.value, item.id, 'phone')
+                    }
+                    fontSize={12}
+                  />
+                  {item?.phone !== '' && item?.phError !== '' && (
+                    <FormErrorMessage fontSize='xs'>
+                      {item?.phError}
+                    </FormErrorMessage>
+                  )}
+                </FormControl>
+                <IconButton
+                  border='1px solid'
+                  colorScheme='white'
+                  borderColor={borderColor}
+                  aria-label='Remove config'
+                  onClick={() => deleteRow(item)}
+                  icon={
+                    <Icon color={'#E53E3E'} w={6} h={6} as={MdDeleteOutline} />
+                  }
+                />
+              </Flex>
+            ))}
         </Flex>
-        <Button
-          aria-label='Add config'
-          onClick={addRow}
-          colorScheme='blue'
-          leftIcon={<FaPlus />}
-          fontWeight={'medium'}
-          paddingLeft={'2px'}
-          fontSize={'sm'}
-          variant='link'
-        >
-          Add New
-        </Button>
-        {error !== '' && <LynkAlert msg={error} />}
-      </LynkModal>
-    </>
+      </Flex>
+      <Button
+        my={3}
+        aria-label='Add config'
+        onClick={addRow}
+        colorScheme='blue'
+        leftIcon={<FaPlus />}
+        fontWeight={'medium'}
+        paddingLeft={'2px'}
+        fontSize={'sm'}
+        variant='link'
+      >
+        Add New
+      </Button>
+      {error !== '' && <LynkAlert msg={error} />}
+    </LynkModal>
   )
 }
 
