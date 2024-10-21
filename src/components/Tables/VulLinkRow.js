48db26d85fa506898460c98afd82e5d499440ce8
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-10-21 08:36:15 +0530

    Update status history view (#4625)

--- src/components/Tables/VulLinkRow.js
+++ src/components/Tables/VulLinkRow.js
@@ -1,6 +1,14 @@
+import React from 'react'
 import { getFullDateAndTime, timeSince } from 'utils'
 
-import { IconButton, Td, Text, Tooltip, Tr } from '@chakra-ui/react'
+import {
+  IconButton,
+  Td,
+  Text,
+  Tooltip,
+  Tr,
+  useDisclosure
+} from '@chakra-ui/react'
 
 import VexInfoDrawer from 'components/Drawer/VexInfoDrawer'
 
@@ -8,7 +16,7 @@ import { useThemeColor } from 'hooks/useThemeColors'
 
 import { FaEye } from 'react-icons/fa6'
 
-const VulLinkRow = ({ key, data }) => {
+const VulLinkRow = ({ key, data, vulnId }) => {
   const { primaryTextColor, headingTextColor } = useThemeColor([
     'primaryTextColor',
     'headingTextColor'
@@ -16,33 +24,45 @@ const VulLinkRow = ({ key, data }) => {
 
   const { changedBy, status, updatedAt } = data || ''
 
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
-    <Tr key={key} border={'none'}>
-      <Td pl={0} py={2.5}>
-        <Text fontSize={'sm'} color={primaryTextColor} mb={0.5}>
-          {status}
-        </Text>
-        <Tooltip label={getFullDateAndTime(updatedAt)}>
-          <Text fontSize={'xs'} color={'gray.500'}>
-            {timeSince(updatedAt)}
+    <>
+      <Tr key={key} border={'none'}>
+        <Td pl={0} py={2.5}>
+          <Text fontSize={'sm'} color={primaryTextColor} mb={0.5}>
+            {status}
           </Text>
-        </Tooltip>
-      </Td>
-      <Td py={2.5}>
-        <Text fontSize={13} color={'gray.500'}>
-          {changedBy}
-        </Text>
-      </Td>
-      <Td pr={0} isNumeric py={2.5}>
-        <Tooltip placement='left' label={<VexInfoDrawer data={data} />}>
+          <Tooltip label={getFullDateAndTime(updatedAt)}>
+            <Text fontSize={'xs'} color={'gray.500'}>
+              {timeSince(updatedAt)}
+            </Text>
+          </Tooltip>
+        </Td>
+        <Td py={2.5}>
+          <Text fontSize={13} color={'gray.500'}>
+            {changedBy}
+          </Text>
+        </Td>
+        <Td pr={0} isNumeric py={2.5}>
           <IconButton
             size='sm'
             variant='outline'
+            onClick={onOpen}
             icon={<FaEye color={headingTextColor} />}
           />
-        </Tooltip>
-      </Td>
-    </Tr>
+        </Td>
+      </Tr>
+
+      {isOpen && (
+        <VexInfoDrawer
+          vulnId={vulnId}
+          data={data}
+          isOpen={isOpen}
+          onClose={onClose}
+        />
+      )}
+    </>
   )
 }
 
