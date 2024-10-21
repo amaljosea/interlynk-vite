f4376d62697e6d04bbd1f27ea2107ee881a859cc
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-10-21 18:22:36 +0530

    Update device warning view (#4646)

--- src/components/DeviceWarning.js
+++ src/components/DeviceWarning.js
@@ -1,11 +1,14 @@
 import InterlynkLogo from 'assets/img/logo.png'
 
 import { Box, Button, Flex, Heading, Img, Text } from '@chakra-ui/react'
+import { useColorMode } from '@chakra-ui/react'
 
 import { useThemeColor } from 'hooks/useThemeColors'
 
 const DeviceWarning = () => {
   const { primaryBgColor } = useThemeColor(['primaryBgColor'])
+  const { colorMode } = useColorMode()
+  const isDark = colorMode === 'dark'
 
   return (
     <Box w='100%' h='100vh' bgGradient='linear(to-br, #4299e1, #1A365D)'>
@@ -15,7 +18,10 @@ const DeviceWarning = () => {
           sx={{ w: '40px', h: '40px' }}
           filter={'brightness(0) invert(1)'}
         />
-        <Text fontSize={'2xl'} color={primaryBgColor} fontWeight={600}>
+        <Text
+          sx={{ fontSize: '2xl', fontWeight: 600 }}
+          color={isDark ? 'gray.100' : primaryBgColor}
+        >
           Interlynk
         </Text>
       </Flex>
@@ -25,7 +31,7 @@ const DeviceWarning = () => {
       >
         <Heading
           textAlign='center'
-          sx={{ fontSize: '3xl', opacity:0.9,  color: 'gray.200' }}
+          sx={{ fontSize: '3xl', opacity: 0.9, color: 'gray.200' }}
         >
           This page is best viewed on Desktop
         </Heading>
