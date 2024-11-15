6ae72e42ba65b0cc0d401e4220b4d276a5b9ec3b
Amal Jose Alex <howareyouami@gmail.com>
2024-11-15 18:54:27 +0530

    Relationship delete modal bug fix (#4898)

--- src/components/RelDeleteModal.js
+++ src/components/RelDeleteModal.js
@@ -1,12 +1,16 @@
 import ConfirmationModal from 'views/Dashboard/Products/components/ConfirmationModal'
 
 const RelDeleteModal = ({ isOpen, onClose, activeComp, handleRemove }) => {
+  const componentName = `${activeComp?.toComp?.name}${
+    activeComp?.toComp?.version ? ` - ${activeComp.toComp.version}` : ''
+  }`
+
   return (
     <ConfirmationModal
       isOpen={isOpen}
       onClose={onClose}
       onConfirm={handleRemove}
-      name={`${activeComp?.toComp.name}-${activeComp?.toComp.version}`}
+      name={componentName}
       title='Remove'
       description='This will remove the relationship of this component with other
               components and change the dependency order of this version.'
