c86f4c96d9d2f22178c39b63d217b3fe989e5adf
Amal Jose Alex <howareyouami@gmail.com>
2024-11-18 19:26:19 +0530

    Reducing chances of UI freeze for large SBOM (#4916)

--- src/utils/getComponentHealthScoreFromLocalData.js
+++ src/utils/getComponentHealthScoreFromLocalData.js
@@ -1,15 +1,31 @@
 import { getComponentHealthScore } from './getComponentHealthScore'
 import { healthScoreData } from './healthScoreData'
 
-export const getComponentHealthScoreFromLocalData = ({
+const fetchHealthScoreData = async () => {
+  // Simulate async data loading so that componentData is available for getComponentHealthScore, reducing chances of UI freeze
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(healthScoreData)
+    }, 1000)
+  })
+}
+
+export const getComponentHealthScoreFromLocalData = async ({
   componentName,
   componentVersion
 }) => {
-  const componentData = healthScoreData.find(
+  const data = await fetchHealthScoreData()
+  const componentData = data.find(
     (item) =>
       item.ComponentName === componentName &&
       item.ComponentVersion === componentVersion
   )
+  if (!componentData) {
+    console.warn(
+      `Missing data for component ${componentName} v${componentVersion}`
+    )
+    return { healthScore: 0 }
+  }
 
   const { healthScore } = getComponentHealthScore(componentData)
 
