d0cff95584b4b6ba2646fe67a8e0eee9ef5e98cd
Amal Jose Alex <howareyouami@gmail.com>
2024-11-20 18:01:39 +0530

    Replaced styled-components and used Chakra components for consistency (#4936)
    * Replaced styled-components and used Chakra components for consistency
    
    * Replaced emotion/styled and used Chakra components for consistency
    
    * Replaced emotion/styled in CompInsights component
    
    * undo

--- src/components/Drawer/VexInfo.js
+++ src/components/Drawer/VexInfo.js
@@ -1,4 +1,3 @@
-import styled from 'styled-components'
 import { timeSince } from 'utils'
 
 import { Box, SimpleGrid, Text } from '@chakra-ui/react'
@@ -6,62 +5,69 @@ import { Box, SimpleGrid, Text } from '@chakra-ui/react'
 import { useThemeColor } from 'hooks/useThemeColors'
 
 const VexInfo = ({ data, fields }) => {
-  const { primaryTextColor } = useThemeColor(['primaryTextColor'])
+  const { primaryTextColor, sameSecondaryText } = useThemeColor([
+    'primaryTextColor',
+    'sameSecondaryText'
+  ])
 
-  const CustomText = styled(Text)`
-    font-size: 13px;
-    color: #718096;
-    letter-spacing: 0.6px;
-  `
+  const labelStyle = {
+    fontSize: '13px',
+    color: sameSecondaryText,
+    letterSpacing: '0.6px'
+  }
 
-  const style = { color: primaryTextColor, mt: 1, fontSize: 14 }
+  const valueStyle = {
+    color: primaryTextColor,
+    mt: 1,
+    fontSize: '14px'
+  }
 
   return (
     <SimpleGrid columns={2} gap={4} pt={2} px={4}>
       <Box>
-        <CustomText>Status</CustomText>
-        <Text sx={style}>{data?.status || 'N/A'}</Text>
+        <Text sx={labelStyle}>Status</Text>
+        <Text sx={valueStyle}>{data?.status || 'N/A'}</Text>
       </Box>
       <Box>
-        <CustomText>Justification</CustomText>
-        <Text sx={style}>{data?.justification || 'N/A'}</Text>
+        <Text sx={labelStyle}>Justification</Text>
+        <Text sx={valueStyle}>{data?.justification || 'N/A'}</Text>
       </Box>
       <Box>
-        <CustomText>Impact Statement</CustomText>
-        <Text sx={style}>{data?.impact || 'N/A'}</Text>
+        <Text sx={labelStyle}>Impact Statement</Text>
+        <Text sx={valueStyle}>{data?.impact || 'N/A'}</Text>
       </Box>
       <Box>
-        <CustomText>Internal Notes</CustomText>
-        <Text sx={style}>{data?.note || 'N/A'}</Text>
+        <Text sx={labelStyle}>Internal Notes</Text>
+        <Text sx={valueStyle}>{data?.note || 'N/A'}</Text>
       </Box>
       <Box>
-        <CustomText>Action Statement</CustomText>
-        <Text sx={style}>{data?.actionStmt || 'N/A'}</Text>
+        <Text sx={labelStyle}>Action Statement</Text>
+        <Text sx={valueStyle}>{data?.actionStmt || 'N/A'}</Text>
       </Box>
       <Box>
-        <CustomText>Fixed Version</CustomText>
-        <Text sx={style}>{data?.fixedIn || 'N/A'}</Text>
+        <Text sx={labelStyle}>Fixed Version</Text>
+        <Text sx={valueStyle}>{data?.fixedIn || 'N/A'}</Text>
       </Box>
       <Box>
-        <CustomText>Details</CustomText>
-        <Text sx={style}>{data?.detail || 'N/A'}</Text>
+        <Text sx={labelStyle}>Details</Text>
+        <Text sx={valueStyle}>{data?.detail || 'N/A'}</Text>
       </Box>
       {fields?.length > 0 &&
-        fields?.map((item) => (
+        fields.map((item) => (
           <Box key={item?.id}>
-            <CustomText>
+            <Text sx={labelStyle}>
               {item?.componentVulnCustomFieldDefinition?.displayName || ''}
-            </CustomText>
-            <Text sx={style}>{item?.value || ''}</Text>
+            </Text>
+            <Text sx={valueStyle}>{item?.value || ''}</Text>
           </Box>
         ))}
       <Box>
-        <CustomText>Created By</CustomText>
-        <Text sx={style}>{data?.changedBy || 'N/A'}</Text>
+        <Text sx={labelStyle}>Created By</Text>
+        <Text sx={valueStyle}>{data?.changedBy || 'N/A'}</Text>
       </Box>
       <Box>
-        <CustomText>Created On</CustomText>
-        <Text sx={style}>
+        <Text sx={labelStyle}>Created On</Text>
+        <Text sx={valueStyle}>
           {data?.updatedAt ? timeSince(data?.updatedAt) : 'N/A'}
         </Text>
       </Box>
