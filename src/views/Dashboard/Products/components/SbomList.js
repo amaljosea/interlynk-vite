37b8e30ddaa351d4c4260eb5f6ec59c244f116f7
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-11-21 17:57:18 +0530

    Add SBOM protmote warning (#4948)

--- src/views/Dashboard/Products/components/SbomList.js
+++ src/views/Dashboard/Products/components/SbomList.js
@@ -1,4 +1,5 @@
 import { useMutation, useQuery } from '@apollo/client'
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getFullDateAndTime, timeSince, truncatedValue } from 'utils'
 
@@ -9,7 +10,8 @@ import {
   DrawerContent,
   DrawerHeader,
   DrawerOverlay,
-  IconButton
+  IconButton,
+  useDisclosure
 } from '@chakra-ui/react'
 import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 import { Stack, Tag, TagLabel, Text, Tooltip } from '@chakra-ui/react'
@@ -21,9 +23,14 @@ import { GetSbomAlternatives } from 'graphQL/Queries'
 
 import { FaArrowUp } from 'react-icons/fa6'
 
+import ConfirmationModal from './ConfirmationModal'
+
 const SbomList = ({ sbomId, isOpen, onClose }) => {
   const params = useParams()
   const { showToast } = useCustomToast()
+
+  const PROMOTE_WARNING = useDisclosure()
+  const [activeSbom, setActiveSbom] = useState(null)
 
   const columns = [
     'IMPORTED',
@@ -34,7 +41,7 @@ const SbomList = ({ sbomId, isOpen, onClose }) => {
     ''
   ]
 
-  const [updateSbom] = useMutation(sbomUpdate)
+  const [updateSbom, { loading: updateLoading }] = useMutation(sbomUpdate)
 
   const { data: sbomAlts, loading } = useQuery(GetSbomAlternatives, {
     skip: isOpen ? false : true,
@@ -43,6 +50,11 @@ const SbomList = ({ sbomId, isOpen, onClose }) => {
       sbomId: sbomId
     }
   })
+
+  const handleWarning = (item) => {
+    setActiveSbom(item)
+    PROMOTE_WARNING.onOpen()
+  }
 
   const handlePromote = async (item) => {
     await updateSbom({
@@ -67,97 +79,111 @@ const SbomList = ({ sbomId, isOpen, onClose }) => {
   if (loading) return null
 
   return (
-    <Drawer size='xl' isOpen={isOpen} placement='right' onClose={onClose}>
-      <DrawerOverlay />
-      <DrawerContent>
-        <DrawerCloseButton mt={1} />
-        <DrawerHeader borderBottomWidth='1px'>
-          {truncatedValue(sbomAlts?.sbom?.projectVersion, 40) + ' SBOM List'}
-        </DrawerHeader>
-        <DrawerBody>
-          <Table variant='simple' m={0} p={0}>
-            <Thead>
-              <Tr>
-                {columns.map((item, index) => (
-                  <Th px={0} key={index} fontFamily={'inherit'}>
-                    {item}
-                  </Th>
-                ))}
-              </Tr>
-            </Thead>
-            <Tbody>
-              {sbomAlts?.sbom?.alternatives?.length > 0 &&
-                [...sbomAlts.sbom.alternatives]
-                  .sort((a, b) => {
-                    const dateA = new Date(a.updatedAt)
-                    const dateB = new Date(b.updatedAt)
-                    return dateB - dateA
-                  })
-                  .map((item, index) => {
-                    const { createdAt, stats, lifecycle, updatedAt } = item
-                    return (
-                      <Tr key={index}>
-                        <Td px={0} fontSize={'sm'} width='240px'>
-                          <Stack direction={'row'}>
-                            <Text>{getFullDateAndTime(createdAt)}</Text>
-                          </Stack>
-                        </Td>
-                        <Td px={0} fontSize={'sm'} width='130px'>
-                          <Tag
-                            size='md'
-                            variant='subtle'
-                            width={16}
-                            colorScheme={'blue'}
-                          >
-                            <TagLabel mx={'auto'}>{stats?.compCount}</TagLabel>
-                          </Tag>
-                        </Td>
-                        <Td px={0} fontSize={'sm'} width='130px'>
-                          <Tag
-                            size='md'
-                            variant='subtle'
-                            width={16}
-                            colorScheme={'blue'}
-                            mx={'auto'}
-                          >
-                            <TagLabel mx={'auto'}>
-                              {stats?.compLicenseCount}
-                            </TagLabel>
-                          </Tag>
-                        </Td>
-                        <Td px={0} fontSize={'sm'} width='150px'>
-                          <Tag
-                            width={24}
-                            colorScheme='cyan'
-                            textTransform={'capitalize'}
-                          >
-                            <TagLabel mx={'auto'}>{lifecycle}</TagLabel>
-                          </Tag>
-                        </Td>
-                        <Td px={0} fontSize={'sm'}>
-                          <Tooltip
-                            label={getFullDateAndTime(updatedAt)}
-                            placement='top'
-                          >
-                            <Text>{timeSince(updatedAt)}</Text>
-                          </Tooltip>
-                        </Td>
-                        <Td px={0} fontSize={'sm'}>
-                          <IconButton
-                            size='sm'
-                            colorScheme='blue'
-                            icon={<FaArrowUp />}
-                            onClick={() => handlePromote(item)}
-                          />
-                        </Td>
-                      </Tr>
-                    )
-                  })}
-            </Tbody>
-          </Table>
-        </DrawerBody>
-      </DrawerContent>
-    </Drawer>
+    <>
+      <Drawer size='xl' isOpen={isOpen} placement='right' onClose={onClose}>
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton mt={1} />
+          <DrawerHeader borderBottomWidth='1px'>
+            {truncatedValue(sbomAlts?.sbom?.projectVersion, 40) + ' SBOM List'}
+          </DrawerHeader>
+          <DrawerBody>
+            <Table variant='simple' m={0} p={0}>
+              <Thead>
+                <Tr>
+                  {columns.map((item, index) => (
+                    <Th px={0} key={index} fontFamily={'inherit'}>
+                      {item}
+                    </Th>
+                  ))}
+                </Tr>
+              </Thead>
+              <Tbody>
+                {sbomAlts?.sbom?.alternatives?.length > 0 &&
+                  [...sbomAlts.sbom.alternatives]
+                    .sort((a, b) => {
+                      const dateA = new Date(a.updatedAt)
+                      const dateB = new Date(b.updatedAt)
+                      return dateB - dateA
+                    })
+                    .map((item, index) => {
+                      const { createdAt, stats, lifecycle, updatedAt } = item
+                      return (
+                        <Tr key={index}>
+                          <Td px={0} fontSize={'sm'} width='240px'>
+                            <Stack direction={'row'}>
+                              <Text>{getFullDateAndTime(createdAt)}</Text>
+                            </Stack>
+                          </Td>
+                          <Td px={0} fontSize={'sm'} width='130px'>
+                            <Tag
+                              size='md'
+                              variant='subtle'
+                              width={16}
+                              colorScheme={'blue'}
+                            >
+                              <TagLabel mx={'auto'}>
+                                {stats?.compCount}
+                              </TagLabel>
+                            </Tag>
+                          </Td>
+                          <Td px={0} fontSize={'sm'} width='130px'>
+                            <Tag
+                              size='md'
+                              variant='subtle'
+                              width={16}
+                              colorScheme={'blue'}
+                              mx={'auto'}
+                            >
+                              <TagLabel mx={'auto'}>
+                                {stats?.compLicenseCount}
+                              </TagLabel>
+                            </Tag>
+                          </Td>
+                          <Td px={0} fontSize={'sm'} width='150px'>
+                            <Tag
+                              width={24}
+                              colorScheme='cyan'
+                              textTransform={'capitalize'}
+                            >
+                              <TagLabel mx={'auto'}>{lifecycle}</TagLabel>
+                            </Tag>
+                          </Td>
+                          <Td px={0} fontSize={'sm'}>
+                            <Tooltip
+                              label={getFullDateAndTime(updatedAt)}
+                              placement='top'
+                            >
+                              <Text>{timeSince(updatedAt)}</Text>
+                            </Tooltip>
+                          </Td>
+                          <Td px={0} fontSize={'sm'}>
+                            <IconButton
+                              size='sm'
+                              colorScheme='blue'
+                              icon={<FaArrowUp />}
+                              onClick={() => handleWarning(item)}
+                            />
+                          </Td>
+                        </Tr>
+                      )
+                    })}
+              </Tbody>
+            </Table>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+
+      <ConfirmationModal
+        isLoading={updateLoading}
+        title={'Promote to version'}
+        isOpen={PROMOTE_WARNING?.isOpen}
+        onClose={PROMOTE_WARNING?.onClose}
+        onConfirm={() => handlePromote(activeSbom)}
+        name={getFullDateAndTime(activeSbom?.createdAt)}
+        description={`This will promote the existing one with the new one`}
+      />
+    </>
   )
 }
 
