1be10ca1254dd7b03305cb32c2602363a981149c
Amal Jose Alex <howareyouami@gmail.com>
2024-11-29 10:53:06 +0530

    Replaced tooltip with used Chakra tooltip for consistency (#5015)

--- src/components/Tables/ActivitiesOverviewRow.js
+++ src/components/Tables/ActivitiesOverviewRow.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { getFullDateAndTime } from 'utils'
 
-import { Box, Flex, Icon, Stack, Text } from '@chakra-ui/react'
-
-import Tooltip from 'components/Tooltip'
+import { Box, Flex, Icon, Stack, Text, Tooltip } from '@chakra-ui/react'
 
 import { useThemeColor } from 'hooks/useThemeColors'
 
@@ -156,21 +154,24 @@ function ActivitiesOverviewRow(props) {
     }
   }
 
+  const tooltipLabel = action?.charAt(0).toUpperCase() + action?.slice(1)
   return (
     <Flex alignItems='flex-start' minH='78px' justifyContent='start' mb='5px'>
       <Flex direction='column' h='100%'>
-        <Tooltip text={action}>
-          <Icon
-            as={valueToIcon(action, event, orig, updated)}
-            h={'30px'}
-            w={'30px'}
-            pe={'6px'}
-            mx={'-10px'}
-            mr={'10px'}
-            pb={'6px'}
-            zIndex='1'
-            color={valueToColor(action, event, orig, updated)}
-          />
+        <Tooltip label={tooltipLabel} placement='top'>
+          <Box>
+            <Icon
+              as={valueToIcon(action, event, orig, updated)}
+              h={'30px'}
+              w={'30px'}
+              pe={'6px'}
+              mx={'-10px'}
+              mr={'10px'}
+              pb={'6px'}
+              zIndex='1'
+              color={valueToColor(action, event, orig, updated)}
+            />
+          </Box>
         </Tooltip>
         <Box
           w='2px'
