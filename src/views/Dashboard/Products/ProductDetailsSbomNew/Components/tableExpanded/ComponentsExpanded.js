d9ca1beaced4ecbc61b0b5abeff03aae9938cdb4
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2024-12-04 20:50:30 +0530

    Update component expand view (#5059)

--- src/views/Dashboard/Products/ProductDetailsSbomNew/Components/tableExpanded/ComponentsExpanded.js
+++ src/views/Dashboard/Products/ProductDetailsSbomNew/Components/tableExpanded/ComponentsExpanded.js
@@ -3,9 +3,8 @@ import { getFullDateAndTime, getSignedUrlParams, isCustomerView } from 'utils'
 import { parseLicenseString } from 'utils'
 import { openSsf } from 'variables/general'
 
-import { Box, Flex, Text, VStack } from '@chakra-ui/react'
+import { Box, Flex, Tag, Text, VStack } from '@chakra-ui/react'
 import { Grid, GridItem } from '@chakra-ui/react'
-import { Tag } from '@chakra-ui/react'
 
 import { CustomText } from 'components/Misc/CustomText'
 import SupplierTag from 'components/SupplierTag'
@@ -50,214 +49,182 @@ const ExpandedComponent = ({
       workBreak: 'break-all'
     }
     return (
-      <>
-        <Box
-          sx={{ w: '100%', p: 5 }}
-          boxShadow='inset 0px -5px 5px rgba(0, 0, 0, 0.08), inset 0px 5px 5px rgba(0, 0, 0, 0.08)'
-        >
-          <Grid templateColumns='repeat(3, 1fr)' py={2} gap={6}>
-            <GridItem>
-              <CustomText>Name :</CustomText>
-              <Text sx={textStyle} width={'90%'}>
-                {name || 'N/A'}
-              </Text>
-            </GridItem>
-            <GridItem w='100%' colSpan={3}>
-              <CustomText>Description :</CustomText>
-              <Text sx={textStyle} width={'90%'}>
-                {description !== null ? description : 'N/A'}
-              </Text>
-            </GridItem>
-            <GridItem colSpan={3}>
-              <CustomText>Depends On :</CustomText>
-              <Flex mt={2} alignItems={'flex-start'} gap={2} flexWrap={'wrap'}>
-                {dependsOn?.length > 0 ? (
-                  [...dependsOn]
-                    .sort(
-                      (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
-                    )
-                    .map((comp, index) => (
-                      <Tag
-                        key={index}
-                        variant='subtle'
-                        colorScheme={'blue'}
-                        sx={{ p: 1, workBreak: 'break-all' }}
-                        cursor={customerView ? 'inherit' : 'pointer'}
-                        onClick={() =>
-                          !customerView && handleGraphView(comp?.toComp)
-                        }
-                      >
-                        <Text wordBreak={'break-all'}>
-                          {comp.toComp.name}-{comp.toComp.version}
-                        </Text>
-                      </Tag>
-                    ))
-                ) : (
-                  <Text sx={textStyle}>N/A</Text>
-                )}
-              </Flex>
-            </GridItem>
-            <GridItem>
-              <CustomText>Dependency Of :</CustomText>
-              <Flex mt={2} alignItems={'flex-start'} gap={2} flexWrap={'wrap'}>
-                {dependencyOf?.length > 0 ? (
-                  dependencyOf?.map((comp, index) => (
+      <Box
+        sx={{ w: '100%', p: 5 }}
+        boxShadow='inset 0px -5px 5px rgba(0, 0, 0, 0.08), inset 0px 5px 5px rgba(0, 0, 0, 0.08)'
+      >
+        <Grid templateColumns='repeat(3, 1fr)' py={2} gap={6}>
+          <GridItem colSpan={3}>
+            <CustomText>Name :</CustomText>
+            <Text sx={textStyle} width={'90%'}>
+              {name || 'N/A'}
+            </Text>
+          </GridItem>
+          <GridItem colSpan={3}>
+            <CustomText>Description :</CustomText>
+            <Text sx={textStyle} width={'90%'}>
+              {description !== null ? description : 'N/A'}
+            </Text>
+          </GridItem>
+          <GridItem colSpan={3}>
+            <CustomText>Depends On :</CustomText>
+            <Flex mt={2} alignItems={'flex-start'} gap={2} flexWrap={'wrap'}>
+              {dependsOn?.length > 0 ? (
+                [...dependsOn]
+                  .sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
+                  .map((comp, index) => (
                     <Tag
-                      padding={1}
                       key={index}
                       variant='subtle'
                       colorScheme={'blue'}
+                      sx={{ p: 1, workBreak: 'break-all' }}
                       cursor={customerView ? 'inherit' : 'pointer'}
                       onClick={() =>
-                        !customerView && handleGraphView(comp?.fromComp)
+                        !customerView && handleGraphView(comp?.toComp)
                       }
                     >
                       <Text wordBreak={'break-all'}>
-                        {comp.fromComp.name}-{comp.fromComp.version}
+                        {comp.toComp.name}-{comp.toComp.version}
                       </Text>
                     </Tag>
                   ))
-                ) : (
-                  <Text sx={textStyle}>N/A</Text>
-                )}
-              </Flex>
-            </GridItem>
-            <GridItem>
-              <CustomText>Type :</CustomText>
-              <Text sx={textStyle}>{kind || 'N/A'}</Text>
-            </GridItem>
-            <GridItem>
-              <CustomText>Internal :</CustomText>
-              <Text sx={textStyle}>{internal ? 'True' : 'False'}</Text>
-            </GridItem>
-            <GridItem>
-              <CustomText>Supplier :</CustomText>
-              <VStack spacing={4} mt={1} alignItems={'left'}>
-                {!customerView ? (
-                  !signedUrlParams && suppliers?.length > 0 ? (
-                    <>
-                      {suppliers.map((item, index) => (
-                        <SupplierTag
-                          key={index}
-                          item={item}
-                          editable={false}
-                          premission={isArchived}
-                          onDelete={() => onDeleteSup(item)}
-                        />
-                      ))}
-                    </>
-                  ) : (
-                    <Text sx={textStyle}>N/A</Text>
-                  )
-                ) : suppliers?.length > 0 ? (
+              ) : (
+                <Text sx={textStyle}>N/A</Text>
+              )}
+            </Flex>
+          </GridItem>
+          <GridItem>
+            <CustomText>Dependency Of :</CustomText>
+            <Flex mt={2} alignItems={'flex-start'} gap={2} flexWrap={'wrap'}>
+              {dependencyOf?.length > 0 ? (
+                dependencyOf?.map((comp, index) => (
+                  <Tag
+                    padding={1}
+                    key={index}
+                    variant='subtle'
+                    colorScheme={'blue'}
+                    cursor={customerView ? 'inherit' : 'pointer'}
+                    onClick={() =>
+                      !customerView && handleGraphView(comp?.fromComp)
+                    }
+                  >
+                    <Text wordBreak={'break-all'}>
+                      {comp.fromComp.name}-{comp.fromComp.version}
+                    </Text>
+                  </Tag>
+                ))
+              ) : (
+                <Text sx={textStyle}>N/A</Text>
+              )}
+            </Flex>
+          </GridItem>
+          <GridItem>
+            <CustomText>Type :</CustomText>
+            <Text sx={textStyle}>{kind || 'N/A'}</Text>
+          </GridItem>
+          <GridItem>
+            <CustomText>Internal :</CustomText>
+            <Text sx={textStyle}>{internal ? 'True' : 'False'}</Text>
+          </GridItem>
+          <GridItem>
+            <CustomText>Supplier :</CustomText>
+            <VStack spacing={4} mt={1} alignItems={'left'}>
+              {!customerView ? (
+                !signedUrlParams && suppliers?.length > 0 ? (
                   <>
                     {suppliers.map((item, index) => (
-                      <SupplierTag key={index} item={item} editable={false} />
+                      <SupplierTag
+                        key={index}
+                        item={item}
+                        editable={false}
+                        premission={isArchived}
+                        onDelete={() => onDeleteSup(item)}
+                      />
                     ))}
                   </>
                 ) : (
                   <Text sx={textStyle}>N/A</Text>
-                )}
-              </VStack>
-            </GridItem>
-            <GridItem>
-              <CustomText>PURL :</CustomText>
+                )
+              ) : suppliers?.length > 0 ? (
+                <>
+                  {suppliers.map((item, index) => (
+                    <SupplierTag key={index} item={item} editable={false} />
+                  ))}
+                </>
+              ) : (
+                <Text sx={textStyle}>N/A</Text>
+              )}
+            </VStack>
+          </GridItem>
+          <GridItem>
+            <CustomText>PURL :</CustomText>
+            <Text
+              sx={textStyle}
+              cursor={'pointer'}
+              onClick={() => onCheckPurl(data)}
+            >
+              {purl !== null && purl !== '' ? decodeURI(purl) : 'N/A'}
+            </Text>
+          </GridItem>
+          <GridItem>
+            <CustomText>CPES :</CustomText>
+            {cpes?.length > 0 ? (
               <Text
                 sx={textStyle}
                 cursor={'pointer'}
-                onClick={() => onCheckPurl(data)}
+                onClick={() => onCheckCpe(data)}
               >
-                {purl !== null && purl !== '' ? decodeURI(purl) : 'N/A'}
+                {cpes[0]}
               </Text>
-            </GridItem>
-            <GridItem>
-              <CustomText>CPES :</CustomText>
-              <Flex
-                flexDirection={'column'}
-                sx={{ gap: 1, flexWrap: 'wrap', alignItems: 'flex-start' }}
-              >
-                {cpes?.length > 0 &&
-                  cpes.map((item, index) => (
-                    <Text
-                      key={index}
-                      sx={textStyle}
-                      cursor={'pointer'}
-                      onClick={() => onCheckCpe(data)}
-                    >
-                      {item}
-                    </Text>
-                  ))}
-              </Flex>
-            </GridItem>
-            <GridItem>
-              <CustomText>Scope :</CustomText>
-              <Text sx={textStyle} textTransform={'capitalize'}>
-                {scope || 'N/A'}
-              </Text>
-            </GridItem>
-            <GridItem>
-              <CustomText>Licenses :</CustomText>
-              <Flex alignItems={'center'} gap={2} flexWrap={'wrap'} my={2}>
-                {/* SPDX */}
-                {licenses?.length > 0 &&
-                  licenses.map((item, index) => (
-                    <Tag
-                      key={index}
-                      size={'md'}
-                      variant='subtle'
-                      colorScheme='green'
-                      sx={{ py: 1, w: 'fit-content' }}
-                    >
-                      <Text wordBreak={'break-all'}>{item}</Text>
-                    </Tag>
-                  ))}
-                {/* EXPRESSION */}
-                {licensesExp && licensesExp !== '' && (
-                  <Tag
-                    size={'md'}
-                    variant='subtle'
-                    colorScheme='green'
-                    sx={{ py: 1, w: 'fit-content' }}
-                  >
-                    <Text wordBreak={'break-all'}>
-                      {parseLicenseString(licensesExp)}
-                    </Text>
-                  </Tag>
-                )}
-                {/* CUSTOM */}
-                {licensesCustom?.length > 0 &&
-                  licensesCustom?.map((item, index) => (
-                    <Tag
-                      key={index}
-                      size={'md'}
-                      variant='subtle'
-                      colorScheme='green'
-                      sx={{ py: 1, w: 'fit-content' }}
-                    >
-                      <Text wordBreak={'break-all'}>{item}</Text>
-                    </Tag>
-                  ))}
-              </Flex>
-            </GridItem>
-            <GridItem>
-              <CustomText>OpenSSF Scorecard :</CustomText>
-              <Tag mt={1.5} variant='subtle' colorScheme={'blue'}>
-                {openSSF?.score || 'N/A'}
-              </Tag>
-            </GridItem>
-            <GridItem>
-              <CustomText>Support Level :</CustomText>
-              <Text sx={textStyle}>{supportLevel || 'N/A'}</Text>
-            </GridItem>
-            <GridItem>
-              <CustomText>End-of-Support Date :</CustomText>
-              <Text sx={textStyle}>
-                {endOfSupport ? getFullDateAndTime(endOfSupport) : 'N/A'}
-              </Text>
-            </GridItem>
-          </Grid>
-        </Box>
-      </>
+            ) : (
+              <Text sx={textStyle}>N/A</Text>
+            )}
+          </GridItem>
+          <GridItem>
+            <CustomText>Scope :</CustomText>
+            <Text sx={textStyle} textTransform={'capitalize'}>
+              {scope || 'N/A'}
+            </Text>
+          </GridItem>
+          <GridItem>
+            <CustomText>Licenses :</CustomText>
+            <Flex alignItems={'center'} gap={2} flexWrap={'wrap'} my={2}>
+              {/* SPDX */}
+              {licenses?.length > 0 &&
+                licenses.map((item, index) => (
+                  <Text key={index} sx={textStyle}>
+                    {item}
+                  </Text>
+                ))}
+              {/* EXPRESSION */}
+              {licensesExp && licensesExp !== '' && (
+                <Text sx={textStyle}>{parseLicenseString(licensesExp)}</Text>
+              )}
+              {/* CUSTOM */}
+              {licensesCustom?.length > 0 &&
+                licensesCustom?.map((item, index) => (
+                  <Text key={index} sx={textStyle}>
+                    {item}
+                  </Text>
+                ))}
+            </Flex>
+          </GridItem>
+          <GridItem>
+            <CustomText>OpenSSF Scorecard :</CustomText>
+            <Text sx={textStyle}>{openSSF?.score || 'N/A'}</Text>
+          </GridItem>
+          <GridItem>
+            <CustomText>Support Level :</CustomText>
+            <Text sx={textStyle}>{supportLevel || 'N/A'}</Text>
+          </GridItem>
+          <GridItem>
+            <CustomText>End-of-Support Date :</CustomText>
+            <Text sx={textStyle}>
+              {endOfSupport ? getFullDateAndTime(endOfSupport) : 'N/A'}
+            </Text>
+          </GridItem>
+        </Grid>
+      </Box>
     )
   }, [
     primaryTextColor,
