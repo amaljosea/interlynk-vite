a8e64864c9cea61b38cc6a77c61654b0dc496fce
Amal Jose Alex <howareyouami@gmail.com>
2024-12-11 10:21:41 +0530

    Converted class component to functional component (#5104)

--- src/views/Dashboard/Analytics/SingleGraph.js
+++ src/views/Dashboard/Analytics/SingleGraph.js
@@ -1,4 +1,3 @@
-import { PureComponent } from 'react'
 import {
   CartesianGrid,
   Legend,
@@ -12,29 +11,25 @@ import {
 
 import { useThemeColor } from 'hooks/useThemeColors'
 
-class CustomizedAxisTick extends PureComponent {
-  render() {
-    const { x, y, payload, xOffset } = this.props
-    return (
-      <g transform={`translate(${x},${y})`}>
-        <text
-          x={xOffset}
-          y={0}
-          dy={16}
-          textAnchor='end'
-          // eslint-disable-next-line
-          fill='#666'
-          transform='scale(.7)'
-        >
-          {payload.value}
-        </text>
-      </g>
-    )
-  }
-}
+const CustomizedAxisTick = ({ x, y, payload, xOffset = 0 }) => (
+  <g transform={`translate(${x},${y})`}>
+    <text
+      x={xOffset}
+      y={0}
+      dy={16}
+      textAnchor='end'
+      // eslint-disable-next-line
+      fill='#666'
+      transform='scale(.7)'
+    >
+      {payload.value}
+    </text>
+  </g>
+)
 
 export const SingleGraph = ({ data, lines }) => {
   const { primaryBlueText } = useThemeColor(['primaryBlueText'])
+
   return (
     <ResponsiveContainer aspect={2.5} debounce={300}>
       <LineChart data={data}>
@@ -46,17 +41,15 @@ export const SingleGraph = ({ data, lines }) => {
           labelStyle={{ color: '#4A5568' }}
         />
         <Legend wrapperStyle={{ fontSize: 14 }} />
-        {lines.map((item) => {
-          return (
-            <Line
-              key={item.name}
-              type='monotone'
-              stroke={primaryBlueText}
-              activeDot={{ r: 8 }}
-              {...item}
-            />
-          )
-        })}
+        {lines.map((item) => (
+          <Line
+            key={item.name}
+            type='monotone'
+            stroke={primaryBlueText}
+            activeDot={{ r: 8 }}
+            {...item}
+          />
+        ))}
       </LineChart>
     </ResponsiveContainer>
   )
