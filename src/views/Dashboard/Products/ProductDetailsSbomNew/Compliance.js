e57c89008fd002cf550845d9453083959a9d95bb
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2025-01-13 18:50:39 +0530

    Fixed SBOM compliance list order (#5334)

--- src/views/Dashboard/Products/ProductDetailsSbomNew/Compliance.js
+++ src/views/Dashboard/Products/ProductDetailsSbomNew/Compliance.js
@@ -67,81 +67,81 @@ const Compliance = ({ sbomData }) => {
   const fda = fdaData?.length > 0 ? fdaData[0] : []
   const ntia = ntiaData?.length > 0 ? ntiaData[0] : []
 
-  const data = [
-    {
-      id: 1,
-      type: 'ntia',
-      icon: (
-        <Img
-          alt='NTIA'
-          src={NTIA}
-          mr={'auto'}
-          height={'46px'}
-          objectFit={'contain'}
-        />
-      ),
-      title: 'NTIA Minimum Elements',
-      description: `The NTIA (National Telecommunications and Information Administration) Minimum Elements for a Software Bill of Materials (SBOM) are a set of guidelines and recommendations that define the essential information an SBOM should contain.`,
-      url: 'https://www.ntia.doc.gov/files/ntia/publications/sbom_minimum_elements_report.pdf',
-      score: ntiaLoading ? (
-        <Spinner size='xs' />
-      ) : (
-        `${Math.round(ntia?.score)} %`
-      )
-    },
-    {
-      id: 2,
-      type: 'fda',
-      icon: (
-        <Img
-          alt='FDA'
-          src={FDA}
-          mr={'auto'}
-          height={'44px'}
-          objectFit={'contain'}
-        />
-      ),
-      title: 'FDA Cybersecurity Compliance',
-      description:
-        'SBOM requirements from FDA issued the final guidance Cybersecurity in Medical Devices: Quality System Considerations and Content of Premarket Submissions.',
-      url: 'https://www.fda.gov/media/119933/download',
-      score: fdaLoading ? <Spinner size='xs' /> : `${Math.round(fda?.score)} %`
-    },
-    {
-      id: 3,
-      type: 'bsi',
-      icon: (
-        <Img
-          alt='BSI'
-          src={BSI}
-          mr={'auto'}
-          height={'46px'}
-          objectFit={'contain'}
-        />
-      ),
-      title: 'BSI TR-03183',
-      description:
-        'The Technical Guideline TR-03183: Cyber Resilience Requirements for Manufacturers and Products aims to provide manufacturers with advance access to the type of requirements that will be imposed on them by the future Cyber Resilience Act (CRA) of the EU.',
-      url: 'https://www.bsi.bund.de/SharedDocs/Downloads/EN/BSI/Publications/TechGuidelines/TR03183/BSI-TR-03183-2.pdf?__blob=publicationFile&v=5',
-      score: ''
-    }
-  ]
-
   const onCheck = (index) => {
     setActiveTab(index)
     onOpen()
   }
 
-  const filterData = activeCompliances
-    ?.filter((item) => item?.complianceType !== 'unspecified')
-    ?.map((item) => {
-      const activeItems = data?.find(
-        (row) => row?.type === item?.complianceType
-      )
-      return activeItems
-    })
+  const filterData = activeCompliances?.filter(
+    (item) => item?.complianceType !== 'unspecified'
+  )
 
   const tabs = filterData?.map((item) => item?.type)
+
+  const getScore = (type) => {
+    switch (type) {
+      case 'fda':
+        return fdaLoading ? (
+          <Spinner size='xs' />
+        ) : (
+          `${fda?.score === '0 %' || fda?.score === 'NaN %' ? 'N/A' : Math.round(fda?.score)} %`
+        )
+      case 'ntia':
+        return ntiaLoading ? (
+          <Spinner size='xs' />
+        ) : (
+          `${ntia?.score === '0 %' || ntia?.score === 'NaN %' ? 'N/A' : Math.round(ntia?.score)} %`
+        )
+      case 'bsi':
+        return ''
+    }
+  }
+
+  const getTitle = (type) => {
+    switch (type) {
+      case 'fda':
+        return 'FDA Cybersecurity Compliance'
+      case 'ntia':
+        return 'NTIA Minimum Elements'
+      case 'bsi':
+        return 'BSI TR-03183'
+    }
+  }
+
+  const getDesc = (type) => {
+    switch (type) {
+      case 'fda':
+        return 'SBOM requirements from FDA issued the final guidance Cybersecurity in Medical Devices: Quality System Considerations and Content of Premarket Submissions.'
+      case 'ntia':
+        return 'The NTIA (National Telecommunications and Information Administration) Minimum Elements for a Software Bill of Materials (SBOM) are a set of guidelines and recommendations that define the essential information an SBOM should contain.'
+      case 'bsi':
+        return 'The Technical Guideline TR-03183: Cyber Resilience Requirements for Manufacturers and Products aims to provide manufacturers with advance access to the type of requirements that will be imposed on them by the future Cyber Resilience Act (CRA) of the EU.'
+    }
+  }
+
+  const getIcon = (type) => {
+    switch (type) {
+      case 'fda':
+        return FDA
+      case 'ntia':
+        return NTIA
+      case 'bsi':
+        return BSI
+    }
+  }
+
+  const getLink = (type) => {
+    switch (type) {
+      case 'fda':
+        return 'https://www.fda.gov/media/119933/download'
+      case 'ntia':
+        return 'https://www.ntia.doc.gov/files/ntia/publications/sbom_minimum_elements_report.pdf'
+      case 'bsi':
+        return 'https://www.bsi.bund.de/SharedDocs/Downloads/EN/BSI/Publications/TechGuidelines/TR03183/BSI-TR-03183-2.pdf?__blob=publicationFile&v=5'
+      default:
+        return '#'
+    }
+  }
 
   if (loading) {
     return (
@@ -157,32 +157,44 @@ const Compliance = ({ sbomData }) => {
     <>
       <SimpleGrid columns={3} spacing={5} mt={2}>
         {filterData?.map((item, index) => (
-          <Card key={item?.id} gap={6} border={`1px solid ${grayBorderColor}`}>
+          <Card key={index} gap={6} border={`1px solid ${grayBorderColor}`}>
             <Flex gap={4} alignItems={'center'} justifyContent={'flex-start'}>
-              <Box>{item?.icon}</Box>
+              <Box>
+                <Img
+                  alt='NTIA'
+                  mr={'auto'}
+                  objectFit={'contain'}
+                  src={getIcon(item?.complianceType)}
+                  height={item?.complianceType ? '44px' : '46px'}
+                />
+              </Box>
               <Text
                 fontSize={'16px'}
                 cursor={'pointer'}
                 fontWeight={'medium'}
                 _hover={{ color: primaryBlueText }}
               >
-                {item?.title}
+                {getTitle(item?.complianceType) || ''}
               </Text>
             </Flex>
             <Text
               height={28}
-              color={headingTextColor}
               fontSize={'14px'}
               fontWeight={'light'}
-              pt={item?.description === 'Coming soon...' ? 5 : 0}
+              color={headingTextColor}
+              pt={item?.complianceType === 'bsi' ? 5 : 0}
             >
-              {item?.description}
+              {getDesc(item?.complianceType) || ''}
               <chakra.span
-                color={headingTextColor}
                 fontSize={'14px'}
                 fontWeight={'light'}
+                color={headingTextColor}
               >
-                <Link href={item?.url} isExternal color={primaryBlueText}>
+                <Link
+                  href={getLink(item?.complianceType)}
+                  isExternal
+                  color={primaryBlueText}
+                >
                   {' '}
                   Read more
                 </Link>{' '}
@@ -190,19 +202,17 @@ const Compliance = ({ sbomData }) => {
             </Text>
             <Flex alignItems={'center'} justifyContent={'space-between'}>
               <Text fontSize={'sm'} fontWeight={'medium'}>
-                {!item?.score ? 'Coming soon...' : 'Score'}
+                {item?.complianceType === 'bsi' ? 'Coming soon...' : 'Score'}
               </Text>
               <Button
                 size='sm'
                 fontSize={'xs'}
-                hidden={!item?.score}
                 isDisabled={isArchived}
                 title='Compliance score'
                 onClick={() => onCheck(index)}
+                hidden={item?.complianceType === 'bsi'}
               >
-                {item?.score === '0 %' || item?.score === 'NaN %'
-                  ? 'N/A'
-                  : item?.score}
+                {getScore(item?.complianceType)}
               </Button>
             </Flex>
           </Card>
