c49ac289d371d8edd58f47283f435b1cc8bca2bc
Amal Jose Alex <howareyouami@gmail.com>
2025-01-20 10:26:51 +0530

    Loading feedback for refresh button (#5372)

--- src/components/Icons/RefreshBtn.js
+++ src/components/Icons/RefreshBtn.js
@@ -1,22 +1,40 @@
 import { refetchActiveQueries } from 'context/ApolloWrapper'
+import { useState } from 'react'
 
-import { IconButton, Tooltip } from '@chakra-ui/react'
+import { IconButton, Spinner, Tooltip } from '@chakra-ui/react'
+
+import useCustomToast from 'hooks/useCustomToast'
 
 import { TbRefresh } from 'react-icons/tb'
 
 const RefreshBtn = ({ onClick }) => {
+  const [loading, setLoading] = useState(false)
+  const { showToast } = useCustomToast()
+  const refetch = async () => {
+    try {
+      setLoading(true)
+      await refetchActiveQueries()
+    } catch (err) {
+      showToast({
+        description: 'Refetch failed. Please try again',
+        status: 'error'
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
   return (
     <Tooltip label='Refresh'>
       <IconButton
         onClick={() => {
-          refetchActiveQueries()
+          refetch()
           if (onClick) {
             onClick()
           }
         }}
         aria-label='refresh'
         colorScheme='blue'
-        icon={<TbRefresh fontSize={20} />}
+        icon={loading ? <Spinner size='sm' /> : <TbRefresh fontSize={20} />}
       ></IconButton>
     </Tooltip>
   )
