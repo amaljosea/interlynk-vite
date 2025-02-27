86f2ca46725c52e7f53c2764ddd1b1e42ba1dacd
Amal Jose Alex <howareyouami@gmail.com>
2025-02-27 14:15:17 +0530

    Refactor Violations Drawer to use LynkDrawer (#5711)

--- src/components/Drawer/ViolationDrawer.js
+++ src/components/Drawer/ViolationDrawer.js
@@ -3,17 +3,10 @@ import { useParams } from 'react-router-dom'
 import { truncatedValue } from 'utils'
 import { customStyles } from 'utils/styleUtils'
 
-import {
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerContent,
-  DrawerHeader,
-  DrawerOverlay
-} from '@chakra-ui/react'
 import { Box, Stack, Text, Tooltip } from '@chakra-ui/react'
 
 import CustomLoader from 'components/CustomLoader'
+import LynkDrawer from 'components/LynkDrawer'
 import Pagination from 'components/Pagination'
 import RowComponent from 'components/RowComponent'
 
@@ -116,91 +109,88 @@ const ViolationDrawer = ({ policy, activeRow, sbomId, isOpen, onClose }) => {
   const authorExists = subject === 'VERSION_AUTHOR' && nodes?.length > 0
 
   return (
-    <Drawer size={'lg'} isOpen={isOpen} placement='right' onClose={onClose}>
-      <DrawerOverlay />
-      <DrawerContent>
-        <DrawerCloseButton mt={1} />
-        <DrawerHeader borderBottomWidth='1px'>Violations</DrawerHeader>
-        <DrawerBody py={4}>
-          <Stack spacing={3} direction={'column'}>
-            <Stack spacing={0}>
-              <Text color={secondaryTextColor}>Policy:</Text>
-              <Text textTransform={'capitalize'}>
-                {truncatedValue(policy, 30)}
-              </Text>
-            </Stack>
-            <Stack spacing={0}>
-              <Text color={secondaryTextColor}>Condition:</Text>
-              <Text textTransform={'capitalize'}>
-                {category} {name} {operatorWording} {value}
-              </Text>
-            </Stack>
-            {supplierExists &&
-              nodes?.map((item, index) => (
-                <Stack key={index} spacing={0}>
-                  <Text color={secondaryTextColor}>Value:</Text>
-                  {item?.violation?.suppliers?.length > 0 ? (
-                    item?.violation?.suppliers?.map((item, idx) => (
-                      <Text key={idx}>
-                        {`${item?.contactName || ''} (${item?.contactEmail || ''}) - ${item?.name || ''}`}
-                      </Text>
-                    ))
-                  ) : (
-                    <Text>Not Available</Text>
-                  )}
-                </Stack>
-              ))}
-            {authorExists &&
-              nodes?.map((item, index) => (
-                <Stack key={index} spacing={0}>
-                  <Text color={secondaryTextColor}>Value:</Text>
-                  {item?.violation?.authors?.length > 0 ? (
-                    item?.violation?.authors?.map((item, idx) => (
-                      <Text key={idx}>
-                        {idx + 1}: {`${item?.name || ''} ${item?.email || ''}`}
-                      </Text>
-                    ))
-                  ) : (
-                    <Text>Not Available</Text>
-                  )}
-                </Stack>
-              ))}
-            {(subject === 'VERSION_PRIMARY' ||
-              subject === 'SBOM_PRIMARY_COMPONENT_RELATIONSHIPS') &&
-              nodes?.length > 0 && (
-                <Stack spacing={0}>
-                  <Text fontWeight={'semibold'}>Value:</Text>
-                  {nodes[0].violation?.primaryComponent?.name ? (
-                    <Text style={{ textTransform: 'capitalize' }}>
-                      {nodes[0].violation?.primaryComponent?.name}-{' '}
-                      {nodes[0].violation?.primaryComponent?.version}
-                    </Text>
-                  ) : (
-                    <Text>Not Available</Text>
-                  )}
-                </Stack>
+    <LynkDrawer
+      title={'Violations'}
+      size={'lg'}
+      isOpen={isOpen}
+      onClose={onClose}
+      noFooter
+    >
+      <Stack spacing={3} direction={'column'}>
+        <Stack spacing={0}>
+          <Text color={secondaryTextColor}>Policy:</Text>
+          <Text textTransform={'capitalize'}>{truncatedValue(policy, 30)}</Text>
+        </Stack>
+        <Stack spacing={0}>
+          <Text color={secondaryTextColor}>Condition:</Text>
+          <Text textTransform={'capitalize'}>
+            {category} {name} {operatorWording} {value}
+          </Text>
+        </Stack>
+        {supplierExists &&
+          nodes?.map((item, index) => (
+            <Stack key={index} spacing={0}>
+              <Text color={secondaryTextColor}>Value:</Text>
+              {item?.violation?.suppliers?.length > 0 ? (
+                item?.violation?.suppliers?.map((item, idx) => (
+                  <Text key={idx}>
+                    {`${item?.contactName || ''} (${item?.contactEmail || ''}) - ${item?.name || ''}`}
+                  </Text>
+                ))
+              ) : (
+                <Text>Not Available</Text>
               )}
-            <Text color={secondaryTextColor} hidden={category === 'version'}>
-              Violations List:
-            </Text>
-          </Stack>
-          <Box overflowY={'scroll'} hidden={category === 'version'}>
-            <DataTable
-              responsive
-              columns={columns}
-              data={nodes || []}
-              customStyles={customStyles(headingTextColor)}
-              progressPending={loading}
-              progressComponent={<CustomLoader />}
-              persistTableHead
-            />
-          </Box>
-          <Box hidden={category === 'version' || nodes?.length === 0}>
-            <Pagination {...paginationProps} />
-          </Box>
-        </DrawerBody>
-      </DrawerContent>
-    </Drawer>
+            </Stack>
+          ))}
+        {authorExists &&
+          nodes?.map((item, index) => (
+            <Stack key={index} spacing={0}>
+              <Text color={secondaryTextColor}>Value:</Text>
+              {item?.violation?.authors?.length > 0 ? (
+                item?.violation?.authors?.map((item, idx) => (
+                  <Text key={idx}>
+                    {idx + 1}: {`${item?.name || ''} ${item?.email || ''}`}
+                  </Text>
+                ))
+              ) : (
+                <Text>Not Available</Text>
+              )}
+            </Stack>
+          ))}
+        {(subject === 'VERSION_PRIMARY' ||
+          subject === 'SBOM_PRIMARY_COMPONENT_RELATIONSHIPS') &&
+          nodes?.length > 0 && (
+            <Stack spacing={0}>
+              <Text fontWeight={'semibold'}>Value:</Text>
+              {nodes[0].violation?.primaryComponent?.name ? (
+                <Text style={{ textTransform: 'capitalize' }}>
+                  {nodes[0].violation?.primaryComponent?.name}-{' '}
+                  {nodes[0].violation?.primaryComponent?.version}
+                </Text>
+              ) : (
+                <Text>Not Available</Text>
+              )}
+            </Stack>
+          )}
+        <Text color={secondaryTextColor} hidden={category === 'version'}>
+          Violations List:
+        </Text>
+      </Stack>
+      <Box overflowY={'scroll'} hidden={category === 'version'}>
+        <DataTable
+          responsive
+          columns={columns}
+          data={nodes || []}
+          customStyles={customStyles(headingTextColor)}
+          progressPending={loading}
+          progressComponent={<CustomLoader />}
+          persistTableHead
+        />
+      </Box>
+      <Box hidden={category === 'version' || nodes?.length === 0}>
+        <Pagination {...paginationProps} />
+      </Box>
+    </LynkDrawer>
   )
 }
 
