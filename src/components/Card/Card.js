1d7e14f21570d7abe0b968123aa97c227c941442
amaljosea <amaljoseami@gmail.com>
2025-03-05 16:01:30 +0530

    fixed dark color issue

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,15 @@
 import { Box } from '@chakra-ui/react'
 import { useStyleConfig } from '@chakra-ui/system'
 
+import { useThemeColor } from 'hooks/useThemeColors'
+
 function Card(props) {
   const { variant, children, ...rest } = props
   const styles = useStyleConfig('Card', { variant })
   // Pass the computed styles into the `__css` prop
+  const { lightAndDarkBgColor } = useThemeColor(['lightAndDarkBgColor'])
   return (
-    <Box __css={styles} {...rest}>
+    <Box __css={styles} {...rest} bgColor={lightAndDarkBgColor}>
       {children}
     </Box>
   )
