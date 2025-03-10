89317b4f90794271b5a784cfae835ce1fcba6069
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2025-03-10 18:40:06 +0530

    Update SBOM info card in tools page (#5815)

--- src/components/SbomInfo.js
+++ src/components/SbomInfo.js
@@ -1,163 +1,111 @@
 import { getFullDate, truncatedValue } from 'utils'
 
-import {
-  Flex,
-  HStack,
-  Link,
-  Stack,
-  Table,
-  TableContainer,
-  Tag,
-  TagLabel,
-  Tbody,
-  Td,
-  Text,
-  Th,
-  Thead,
-  Tr
-} from '@chakra-ui/react'
+import { Flex, Stack, Text } from '@chakra-ui/react'
+import { Grid, GridItem } from '@chakra-ui/react'
+import { Tag, TagLabel } from '@chakra-ui/react'
+
+import { useThemeColor } from 'hooks/useThemeColors'
+
+import SupplierTag from './SupplierTag'
 
 const SbomInfo = ({ data }) => {
+  const { sameSecondaryText, grayBorderColor } = useThemeColor([
+    'sameSecondaryText',
+    'grayBorderColor'
+  ])
+
+  const label = { color: sameSecondaryText }
+
+  const container = {
+    pb: 3,
+    gap: 5,
+    w: '100%',
+    fontSize: 'sm',
+    templateColumns: `repeat(12, 1fr)`,
+    borderBottom: `1px solid ${grayBorderColor}`
+  }
+
+  const sbomData = [
+    {
+      label: 'Created At',
+      value: <Text>{getFullDate(data?.creationAt)}</Text>
+    },
+    {
+      label: 'Creation Tool',
+      value: (
+        <Flex gap={2} flexWrap={'wrap'} alignItems={'center'}>
+          {data?.tools?.length > 0 ? (
+            data?.tools.map((item, index) => (
+              <Tag key={index} colorScheme='teal'>
+                <TagLabel>
+                  {item?.name} - {truncatedValue(item?.version, 15)}
+                </TagLabel>
+              </Tag>
+            ))
+          ) : (
+            <TagLabel>N/A</TagLabel>
+          )}
+        </Flex>
+      )
+    },
+    {
+      label: 'Author',
+      value: (
+        <Flex gap={2} flexWrap={'wrap'} alignItems={'center'}>
+          {data?.authors.length > 0 ? (
+            data?.authors.map((item, index) => (
+              <Tag key={index} colorScheme='blue' width={'fit-content'}>
+                <TagLabel>
+                  {item.name} {item?.email && `- ${item.email}`}
+                </TagLabel>
+              </Tag>
+            ))
+          ) : (
+            <Tag>N/A</Tag>
+          )}
+        </Flex>
+      )
+    },
+    {
+      label: 'Supplier',
+      value: (
+        <Flex>
+          {data?.suppliers?.length > 0 ? (
+            data?.suppliers.map((item, index) => (
+              <SupplierTag key={index} item={item} editable={false} />
+            ))
+          ) : (
+            <Tag>N/A</Tag>
+          )}
+        </Flex>
+      )
+    },
+    {
+      label: 'Data License',
+      value: (
+        <Tag
+          size={'md'}
+          variant='subtle'
+          colorScheme='green'
+          width={'fit-content'}
+        >
+          <TagLabel>{data?.licensesExp || 'N/A'}</TagLabel>
+        </Tag>
+      )
+    }
+  ]
+
   return (
-    <TableContainer overflowY={'scroll'}>
-      <Table variant='simple'>
-        <Thead>
-          <Tr>
-            <Th></Th>
-            <Th></Th>
-          </Tr>
-        </Thead>
-        <Tbody fontSize={'sm'}>
-          {/* CREATION TOOLS */}
-          <Tr>
-            <Td pl={0}>Creation Tool</Td>
-            <Td>
-              <Flex
-                flexDirection={'row'}
-                alignItems={'flex-start'}
-                flexWrap={'wrap'}
-                gap={2.5}
-              >
-                {data?.tools &&
-                  data?.tools.map((item, index) => (
-                    <Text key={index}>
-                      {item.name} - {truncatedValue(item.version, 15)}
-                    </Text>
-                  ))}
-              </Flex>
-            </Td>
-          </Tr>
-          {/* CREATED AT */}
-          <Tr>
-            <Td pl={0}>Created At</Td>
-            <Td>{getFullDate(data?.creationAt)}</Td>
-          </Tr>
-          {/* AUTHOR */}
-          <Tr>
-            <Td pl={0}>Author</Td>
-            <Td>
-              <Stack spacing={2} direction={'column'}>
-                {data?.authors.length > 0 &&
-                  data?.authors.map((item, index) => (
-                    <Tag
-                      size={'md'}
-                      key={index}
-                      variant='subtle'
-                      colorScheme='blue'
-                      width={'fit-content'}
-                    >
-                      <TagLabel>
-                        {item.name} - {item.email}
-                      </TagLabel>
-                    </Tag>
-                  ))}
-              </Stack>
-            </Td>
-          </Tr>
-          {/* SUPPLOER */}
-          <Tr>
-            <Td pl={0}>Supplier</Td>
-            <Td>
-              <HStack spacing={4}>
-                {data?.suppliers?.length > 0 &&
-                  data?.suppliers.map((item, index) => (
-                    <Tag
-                      size={'md'}
-                      key={index}
-                      variant='subtle'
-                      colorScheme='orange'
-                    >
-                      <TagLabel>
-                        {item.contactName}
-                        {item.contactEmail && ` (${item.contactEmail})`}
-                        {item.url ? (
-                          <Link
-                            href={
-                              item?.url?.startsWith('http')
-                                ? item.url
-                                : `http://${item.url}`
-                            }
-                            isExternal
-                          >
-                            {item.name}
-                          </Link>
-                        ) : (
-                          ` ${item.name}`
-                        )}
-                      </TagLabel>
-                    </Tag>
-                  ))}
-              </HStack>
-            </Td>
-          </Tr>
-          {/* DATA LICENSE */}
-          <Tr>
-            <Td pl={0}>Data License</Td>
-            <Td>
-              <Flex alignItems={'center'} gap={2} flexWrap={'wrap'}>
-                {/* SPDX */}
-                {data.licenses?.length > 0 &&
-                  data.licenses?.map((item, index) => (
-                    <Tag
-                      size={'md'}
-                      key={index}
-                      variant='subtle'
-                      colorScheme='green'
-                      width={'fit-content'}
-                    >
-                      <TagLabel>{item}</TagLabel>
-                    </Tag>
-                  ))}
-                {/* EXPRESSION */}
-                {data.licensesExp && data.licensesExp !== '' && (
-                  <Tag
-                    size={'md'}
-                    variant='subtle'
-                    colorScheme='green'
-                    width={'fit-content'}
-                  >
-                    <TagLabel>{data.licensesExp}</TagLabel>
-                  </Tag>
-                )}
-                {/* CUSTOM */}
-                {data.licensesCustom?.length > 0 &&
-                  data.licensesCustom.map((item, index) => (
-                    <Tag
-                      size={'md'}
-                      key={index}
-                      variant='subtle'
-                      colorScheme='green'
-                      width={'fit-content'}
-                    >
-                      <TagLabel>{item}</TagLabel>
-                    </Tag>
-                  ))}
-              </Flex>
-            </Td>
-          </Tr>
-        </Tbody>
-      </Table>
-    </TableContainer>
+    <Stack spacing={3} mt={8}>
+      {sbomData?.map((item, index) => (
+        <Grid key={index} {...container}>
+          <GridItem colSpan={3}>
+            <Text {...label}>{item?.label}</Text>
+          </GridItem>
+          <GridItem colSpan={9}>{item?.value}</GridItem>
+        </Grid>
+      ))}
+    </Stack>
   )
 }
 
