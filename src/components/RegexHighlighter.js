9f8c1c8e29c233df3cc4abc01795a9ede78ab273
amaljosea <amaljoseami@gmail.com>
2025-03-18 15:19:32 +0530

    Updated dark mode color for regex highlighter

--- src/components/RegexHighlighter.js
+++ src/components/RegexHighlighter.js
@@ -1,9 +1,13 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { coy } from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { coy, duotoneSea } from 'react-syntax-highlighter/dist/esm/styles/prism'
+
+import { useColorMode } from '@chakra-ui/react'
 
 export const RegexHighlighter = ({ children }) => {
+  const { colorMode } = useColorMode()
+  const isDark = colorMode === 'dark'
   return (
-    <SyntaxHighlighter style={coy} language='regex'>
+    <SyntaxHighlighter style={isDark ? duotoneSea : coy} language='regex'>
       {children}
     </SyntaxHighlighter>
   )
