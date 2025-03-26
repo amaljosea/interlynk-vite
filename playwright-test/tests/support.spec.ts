5f174be21f61bfe28934a760aba89c41bdcb24e3
amaljosea <amaljoseami@gmail.com>
2025-03-26 21:18:25 +0530

    fix

--- playwright-test/tests/support.spec.ts
+++ playwright-test/tests/support.spec.ts
@@ -12,6 +12,7 @@ test.beforeEach(async ({ page }) => {
 })
 
 test('Support CRUD functionality', async ({ page }) => {
+  test.setTimeout(120000)
   const pp = new SupportPage(page)
   try {
     await pp.support()
