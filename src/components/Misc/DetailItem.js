69bfd668b9d2690c2c645d108336285410f7b820
abhisekdev <abhisek@sofueled.com>
2025-04-01 15:11:03 +0530

    Update vuln expand view with advisories and cwe data

--- src/components/Misc/DetailItem.js
+++ src/components/Misc/DetailItem.js
@@ -1,4 +1,4 @@
-import { GridItem, Text } from '@chakra-ui/react'
+import { GridItem, Box } from '@chakra-ui/react'
 
 import { useThemeColor } from 'hooks/useThemeColors'
 
@@ -11,9 +11,9 @@ const DetailItem = ({ label, value, children, valueStyle, ...props }) => {
     <GridItem {...props}>
       <CustomText>{label} :</CustomText>
       {children && <GridItem mt={0}>{children}</GridItem>}
-      <Text mt={1} fontSize={14} color={primaryTextColor} {...valueStyle}>
+      <Box mt={1} fontSize={14} color={primaryTextColor} {...valueStyle}>
         {value}
-      </Text>
+      </Box>
     </GridItem>
   )
 }
