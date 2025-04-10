cc7392cc560c69ae198fc80fb608fcf74e626156
amaljosea <amaljoseami@gmail.com>
2025-04-10 19:10:15 +0530

    refactor: improve connection card component structure and readability

--- src/components/Connections/ConnectionCard.js
+++ src/components/Connections/ConnectionCard.js
@@ -7,13 +7,11 @@ import { useThemeColor } from 'hooks/useThemeColors'
 import Card from '../Card/Card'
 
 const ConnectionCard = ({
-  icon: Icon,
   iconSrc,
   name,
   onConfigure,
   isConnected,
-  description,
-  color
+  description
 }) => {
   const { primaryTextColor, secondaryTextInverse } = useThemeColor([
     'primaryTextColor',
@@ -36,16 +34,12 @@ const ConnectionCard = ({
     >
       <Flex direction='column' height='100%' gap={'16px'}>
         <Flex align='center'>
-          {iconSrc ? (
-            <img
-              src={iconSrc}
-              alt={`${name} icon`}
-              width={'40px'}
-              height='40px'
-            />
-          ) : (
-            <Icon color={color} size='40px' />
-          )}
+          <img
+            src={iconSrc}
+            alt={`${name} icon`}
+            width={'40px'}
+            height='40px'
+          />
           <Text
             ml={'16px'}
             noOfLines={1}
@@ -69,7 +63,7 @@ const ConnectionCard = ({
           isDisabled={!canUpdate}
           width={isConnected ? '150px' : '120px'}
           colorScheme={isConnected ? 'blue' : 'gray'}
-          leftIcon={isConnected ? <CheckCircleIcon /> : undefined}
+          leftIcon={isConnected && <CheckCircleIcon />}
         >
           {isConnected ? 'Configured' : 'Configure'}
         </Button>
