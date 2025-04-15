222f3ab05a7015ae114adc363377aa55368da13a
abhisekdev <abhisek@sofueled.com>
2025-04-15 15:51:11 +0530

    Refactor SBOM alternatives drawer

--- src/views/Dashboard/Products/components/SbomList.js
+++ src/views/Dashboard/Products/components/SbomList.js
@@ -1,16 +1,19 @@
 import { useMutation, useQuery } from '@apollo/client'
 import { useState } from 'react'
+import DataTable from 'react-data-table-component'
 import { useParams } from 'react-router-dom'
 import { getFullDate, timeSince, truncatedValue } from 'utils'
+import { customStyles } from 'utils/styleUtils'
 
 import { IconButton, Stack, useDisclosure } from '@chakra-ui/react'
-import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
 import { Tag, TagLabel, Text, Tooltip } from '@chakra-ui/react'
 
+import CustomLoader from 'components/CustomLoader'
 import LynkDrawer from 'components/LynkDrawer'
 
 import useCustomToast from 'hooks/useCustomToast'
 import { useHasPermission } from 'hooks/useHasPermission'
+import { useThemeColor } from 'hooks/useThemeColors'
 
 import { sbomUpdate } from 'graphQL/Mutation'
 import { GetSbomAlternatives } from 'graphQL/Queries'
@@ -31,14 +34,10 @@ const SbomList = ({ sbomId, projectGroup, isOpen, onClose }) => {
     childKey: 'update_sbom'
   })
 
-  const columns = [
-    'COMPONENTS',
-    'LICENSES',
-    'STATUS',
-    'IMPORTED',
-    'UPDATED',
-    ''
-  ]
+  const { primaryTextColor, headingTextColor } = useThemeColor([
+    'primaryTextColor',
+    'headingTextColor'
+  ])
 
   const [updateSbom, { loading: updateLoading }] = useMutation(sbomUpdate)
 
@@ -49,6 +48,7 @@ const SbomList = ({ sbomId, projectGroup, isOpen, onClose }) => {
       sbomId: sbomId
     }
   })
+  const { alternatives } = sbomAlts?.sbom || {}
 
   const handleWarning = (item) => {
     setActiveSbom(item)
@@ -75,8 +75,6 @@ const SbomList = ({ sbomId, projectGroup, isOpen, onClose }) => {
       .finally(() => onClose())
   }
 
-  if (loading) return null
-
   const subtitle = (
     <Stack spacing={1}>
       <Tag w={'fit-content'} colorScheme='blue' py={1.5}>
@@ -88,6 +86,105 @@ const SbomList = ({ sbomId, projectGroup, isOpen, onClose }) => {
     </Stack>
   )
 
+  const columns = [
+    {
+      id: 'COMPONENTS',
+      name: 'COMPONENTS',
+      compact: true,
+      selector: (row) => {
+        const { stats } = row || {}
+        return (
+          <Tag size='md' variant='subtle' width={16} colorScheme={'blue'}>
+            <TagLabel mx={'auto'}>{stats?.compCount}</TagLabel>
+          </Tag>
+        )
+      },
+      wrap: true
+    },
+    {
+      id: 'LICENSES',
+      name: 'LICENSES',
+      selector: (row) => {
+        const { stats } = row || {}
+        return (
+          <Tag
+            size='md'
+            width={16}
+            mx={'auto'}
+            variant='subtle'
+            colorScheme={'blue'}
+          >
+            <TagLabel mx={'auto'}>{stats?.compLicenseCount}</TagLabel>
+          </Tag>
+        )
+      },
+      wrap: true
+    },
+    {
+      id: 'STATUS',
+      name: 'STATUS',
+      selector: (row) => {
+        const { lifecycle } = row || {}
+        return (
+          <Tag width={24} colorScheme='cyan' textTransform={'capitalize'}>
+            <TagLabel mx={'auto'}>{lifecycle}</TagLabel>
+          </Tag>
+        )
+      },
+
+      right: true,
+      wrap: true
+    },
+    {
+      id: 'IMPORTED',
+      name: 'IMPORTED',
+      selector: (row) => {
+        const { createdAt } = row || {}
+        return (
+          <Tooltip label={getFullDate(createdAt)} placement='top'>
+            <Text color={primaryTextColor}>{timeSince(createdAt)}</Text>
+          </Tooltip>
+        )
+      },
+      right: true,
+      wrap: true
+    },
+    {
+      id: 'UPDATED',
+      name: 'UPDATED',
+      selector: (row) => {
+        const { updatedAt } = row || {}
+        return (
+          <Tooltip label={getFullDate(updatedAt)} placement='top'>
+            <Text color={primaryTextColor}>{timeSince(updatedAt)}</Text>
+          </Tooltip>
+        )
+      },
+      right: true,
+      wrap: true
+    },
+    {
+      id: 'ACTIONS',
+      name: 'ACTIONS',
+      compact: true,
+      selector: (row) => (
+        <Tooltip label={'Promote to version'} placement='top'>
+          <IconButton
+            size='sm'
+            colorScheme='blue'
+            icon={<FaArrowUp />}
+            isDisabled={!editSbom}
+            onClick={() => handleWarning(row)}
+          />
+        </Tooltip>
+      ),
+      right: true,
+      wrap: true
+    }
+  ]
+
+  if (loading) return null
+
   return (
     <>
       <LynkDrawer
@@ -98,86 +195,16 @@ const SbomList = ({ sbomId, projectGroup, isOpen, onClose }) => {
         subtitle={subtitle}
         noFooter
       >
-        <Table variant='simple' m={0} p={0}>
-          <Thead>
-            <Tr>
-              {columns.map((item, index) => (
-                <Th px={0} key={index}>
-                  {item}
-                </Th>
-              ))}
-            </Tr>
-          </Thead>
-          <Tbody>
-            {sbomAlts?.sbom?.alternatives?.length > 0 &&
-              [...sbomAlts.sbom.alternatives]
-                .sort((a, b) => {
-                  const dateA = new Date(a.updatedAt)
-                  const dateB = new Date(b.updatedAt)
-                  return dateB - dateA
-                })
-                .map((item, index) => {
-                  const { createdAt, stats, lifecycle, updatedAt } = item
-                  return (
-                    <Tr key={index}>
-                      <Td px={0} fontSize={'sm'} width='130px'>
-                        <Tag
-                          size='md'
-                          variant='subtle'
-                          width={16}
-                          colorScheme={'blue'}
-                        >
-                          <TagLabel mx={'auto'}>{stats?.compCount}</TagLabel>
-                        </Tag>
-                      </Td>
-                      <Td px={0} fontSize={'sm'} width='130px'>
-                        <Tag
-                          size='md'
-                          width={16}
-                          mx={'auto'}
-                          variant='subtle'
-                          colorScheme={'blue'}
-                        >
-                          <TagLabel mx={'auto'}>
-                            {stats?.compLicenseCount}
-                          </TagLabel>
-                        </Tag>
-                      </Td>
-                      <Td px={0} fontSize={'sm'} width='150px'>
-                        <Tag
-                          width={24}
-                          colorScheme='cyan'
-                          textTransform={'capitalize'}
-                        >
-                          <TagLabel mx={'auto'}>{lifecycle}</TagLabel>
-                        </Tag>
-                      </Td>
-                      <Td px={0} fontSize={'sm'}>
-                        <Tooltip label={getFullDate(createdAt)} placement='top'>
-                          <Text>{timeSince(createdAt)}</Text>
-                        </Tooltip>
-                      </Td>
-                      <Td px={0} fontSize={'sm'}>
-                        <Tooltip label={getFullDate(updatedAt)} placement='top'>
-                          <Text>{timeSince(updatedAt)}</Text>
-                        </Tooltip>
-                      </Td>
-                      <Td px={0} fontSize={'sm'}>
-                        <Tooltip label={'Promote to version'} placement='top'>
-                          <IconButton
-                            size='sm'
-                            colorScheme='blue'
-                            icon={<FaArrowUp />}
-                            isDisabled={!editSbom}
-                            onClick={() => handleWarning(item)}
-                          />
-                        </Tooltip>
-                      </Td>
-                    </Tr>
-                  )
-                })}
-          </Tbody>
-        </Table>
+        <DataTable
+          responsive
+          persistTableHead
+          columns={columns}
+          progressPending={loading}
+          data={alternatives}
+          progressComponent={<CustomLoader />}
+          className='data-table-container'
+          customStyles={customStyles(headingTextColor)}
+        />
       </LynkDrawer>
 
       {PROMOTE_WARNING?.isOpen && (
