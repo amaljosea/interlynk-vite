e15f0da24f5d6e8c600d66dc71db8c2285722637
Amal Jose Alex <howareyouami@gmail.com>
2025-04-16 13:44:49 +0530

    feat: create getUniqueAffectedProducts utility function and add unit tests (#6142)

--- src/components/Drawer/VulnProductsDrawer.js
+++ src/components/Drawer/VulnProductsDrawer.js
@@ -2,6 +2,7 @@ import { gql } from '@apollo/client'
 import { useMemo } from 'react'
 import DataTable from 'react-data-table-component'
 import { getFullDate, timeSince } from 'utils'
+import { getUniqueAffectedProducts } from 'utils/getUniqueAffectedProducts'
 import { customStyles } from 'utils/styleUtils'
 
 import { Flex, Tag, Text, Tooltip } from '@chakra-ui/react'
@@ -71,24 +72,7 @@ const VulnProductsDrawer = ({ isOpen, onClose, data }) => {
     }
   )
 
-  const statusResults = useMemo(() => {
-    if (!nodes) return []
-
-    // Create a Map to store unique projectGroup names since components and versions are not listed
-    const uniqueNodes = new Map()
-
-    nodes.forEach((node) => {
-      const projectGroupName =
-        node?.component?.sbom?.project?.projectGroup?.name
-      if (projectGroupName && !uniqueNodes.has(projectGroupName)) {
-        uniqueNodes.set(projectGroupName, node)
-      }
-    })
-
-    return [...uniqueNodes.values()].sort(
-      (a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)
-    )
-  }, [nodes])
+  const statusResults = useMemo(() => getUniqueAffectedProducts(nodes), [nodes])
 
   const columns = [
     // PRODUCTS
