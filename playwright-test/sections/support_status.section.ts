fcc25266b3f061494b070023d2ec8fcf7ea96c80
Amal Jose Alex <howareyouami@gmail.com>
2025-04-26 13:44:51 +0530

    fix: support status e2e test failing (#6236)

--- playwright-test/sections/support_status.section.ts
+++ playwright-test/sections/support_status.section.ts
@@ -56,8 +56,6 @@ export default class SupportStatusSection {
           await this.page.waitForTimeout(2000)
 
           await this.page.getByTestId('rerun_support_analysis').click()
-          await this.page.getByRole('button', { name: 'Yes' }).click()
-          await this.page.waitForTimeout(2000)
 
           await this.page.locator("//button[@aria-label='refresh']").click()
           await this.page.waitForTimeout(2000)
