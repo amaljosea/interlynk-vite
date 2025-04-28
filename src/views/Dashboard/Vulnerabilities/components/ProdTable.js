97e1479f420293f9f22e459cbc17eb38e29a6a3c
abhisekdev <abhisek@sofueled.com>
2025-04-28 12:22:48 +0530

    Update table layout on vulnerability details page

--- src/views/Dashboard/Vulnerabilities/components/ProdTable.js
+++ src/views/Dashboard/Vulnerabilities/components/ProdTable.js
@@ -37,7 +37,10 @@ import VulnFilters from './VulnsFilter'
 const VulnProdTable = ({ vuln, sbomVersions, prodGroups }) => {
   const params = useParams()
   const productGroupId = params?.productgroupid
-  const { primaryTextColor } = useThemeColor(['primaryTextColor'])
+  const { primaryTextColor, secondaryTextColor } = useThemeColor([
+    'primaryTextColor',
+    'secondaryTextColor'
+  ])
 
   const { id, vulnId } = vuln || ''
 
@@ -103,7 +106,7 @@ const VulnProdTable = ({ vuln, sbomVersions, prodGroups }) => {
                 isDisabled={!component?.sbom?.hasConnectedSboms}
               />
             </Tooltip>
-            <Text color={primaryTextColor}>
+            <Text fontSize={14} color={primaryTextColor}>
               {component?.sbom?.project?.projectGroup?.name || ''}
             </Text>
           </Flex>
@@ -119,14 +122,13 @@ const VulnProdTable = ({ vuln, sbomVersions, prodGroups }) => {
       name: 'VERSION',
       selector: (row) => (
         <Tooltip label={row?.component?.sbom?.projectVersion} placement='top'>
-          <Text color={primaryTextColor} my={2} textAlign={'right'}>
+          <Text fontSize={14} color={primaryTextColor} my={2}>
             {row?.component?.sbom?.projectVersion}
           </Text>
         </Tooltip>
       ),
       wrap: true,
-      right: 'true',
-      width: '12%'
+      width: '14%'
     },
     // VULN COMPONENT
     {
@@ -141,13 +143,17 @@ const VulnProdTable = ({ vuln, sbomVersions, prodGroups }) => {
             direction='column'
             alignItems={'flex-start'}
           >
-            <Text color={primaryTextColor}>{component?.name || ''}</Text>
-            <Text color={primaryTextColor}>{component?.version || ''}</Text>
+            <Text fontSize={14} color={primaryTextColor}>
+              {component?.name || 'N/A'}
+            </Text>
+            <Text fontSize={14} color={secondaryTextColor}>
+              {component?.version || 'N/A'}
+            </Text>
           </Stack>
         )
       },
       wrap: true,
-      width: '24%'
+      width: '26%'
     },
     // ENV
     {
@@ -156,7 +162,11 @@ const VulnProdTable = ({ vuln, sbomVersions, prodGroups }) => {
       selector: (row) => {
         const { component } = row
         return (
-          <Text color={primaryTextColor} textTransform={'capitalize'}>
+          <Text
+            fontSize={14}
+            color={primaryTextColor}
+            textTransform={'capitalize'}
+          >
             {component?.sbom?.project?.name || ''}
           </Text>
         )
@@ -172,20 +182,23 @@ const VulnProdTable = ({ vuln, sbomVersions, prodGroups }) => {
         const { vexStatus, isComplete } = row
         return (
           <Tag
-            size='md'
             variant='solid'
-            width={'150px'}
             colorScheme={statusColor(vexStatus?.name || 'Unspecified')}
           >
-            <TagLabel mx={'auto'} as={Flex} gap={2} alignItems='center'>
+            <TagLabel
+              gap={2}
+              as={Flex}
+              mx={'auto'}
+              fontSize={14}
+              alignItems='center'
+            >
               {isComplete === false && <BsCircleHalf />}{' '}
               {vexStatus?.name || 'Unspecified'}
             </TagLabel>
           </Tag>
         )
       },
-      wrap: true,
-      right: 'true'
+      wrap: true
     },
     // UPDATED AT
     {
@@ -196,7 +209,6 @@ const VulnProdTable = ({ vuln, sbomVersions, prodGroups }) => {
           <Text color={primaryTextColor}>{timeSince(row?.updatedAt)}</Text>
         </Tooltip>
       ),
-      width: '12%',
       right: 'true',
       wrap: true
     }
