b40f5e0b95e9cb9fe450e833c4883e1bf0b163d2
abhisekdev <abhisek@sofueled.com>
2025-05-07 10:07:46 +0530

    Include status fields in vulnerability CSV export

--- src/variables/general.js
+++ src/variables/general.js
@@ -1327,7 +1327,12 @@ export const exportCsvTableConfig = {
       'Links',
       'Internal Notes',
       'Advisories',
-      'CWEs'
+      'CWEs',
+      'Justification',
+      'Action Statement',
+      'Impact Statement',
+      'Response',
+      'Fixed Version'
     ],
     mapDataForExport: (data) =>
       data.map((row) => ({
@@ -1357,6 +1362,41 @@ export const exportCsvTableConfig = {
           ? `${(row?.vuln?.vulnInfo?.epssPercentile * 100).toFixed()} %`
           : '0 %',
         KEV: row?.vuln?.vulnInfo?.kev ? 'True' : 'False',
+        Justification: (() => {
+          const justification =
+            row?.componentVulnLogs?.length > 0
+              ? row?.componentVulnLogs?.map((item) => item?.justification)
+              : []
+          return justification?.length > 0 ? justification.join('; ') : 'N/A'
+        })(),
+        'Action Statement': (() => {
+          const actionStmt =
+            row?.componentVulnLogs?.length > 0
+              ? row?.componentVulnLogs?.map((item) => item?.actionStmt)
+              : []
+          return actionStmt?.length > 0 ? actionStmt.join('; ') : 'N/A'
+        })(),
+        'Impact Statement': (() => {
+          const impact =
+            row?.componentVulnLogs?.length > 0
+              ? row?.componentVulnLogs?.map((item) => item?.impact)
+              : []
+          return impact?.length > 0 ? impact.join('; ') : 'N/A'
+        })(),
+        Response: (() => {
+          const response =
+            row?.componentVulnLogs?.length > 0
+              ? row?.componentVulnLogs?.map((item) => item?.response)
+              : []
+          return response?.length > 0 ? response.join('; ') : 'N/A'
+        })(),
+        'Fixed Version': (() => {
+          const fixedIn =
+            row?.componentVulnLogs?.length > 0
+              ? row?.componentVulnLogs?.map((item) => item?.fixedIn)
+              : []
+          return fixedIn?.length > 0 ? fixedIn.join('; ') : 'N/A'
+        })(),
         'Internal Notes': (() => {
           const notes =
             row?.componentVulnLogs?.length > 0
