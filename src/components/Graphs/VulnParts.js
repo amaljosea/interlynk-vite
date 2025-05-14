2109489d11da6471e9d6de9f6d3596a30f318808
abhisekdev <abhisek@sofueled.com>
2025-05-14 13:44:49 +0530

    Fix parts info cards alignment

--- src/components/Graphs/VulnParts.js
+++ src/components/Graphs/VulnParts.js
@@ -215,19 +215,19 @@ const VulnParts = () => {
         <Stack w={'100%'}>
           <Flex
             gap={2}
-            flexDir={sbomParts?.length > 0 ? 'row' : 'column'}
+            flexDir={sbomParts?.length > 0 && !hidden ? 'row' : 'column'}
             justify='space-between'
           >
             <HStack spacing='3'>
               <Icon as={LuBug} color='gray.500' fontSize={24} />
               <Text fontWeight='bold'>Vulnerabilities</Text>
             </HStack>
-            {sbomParts?.length > 0 ? (
+            {sbomParts?.length > 0 && !hidden ? (
               <Text fontWeight='bold' fontSize='lg'>
                 {total}
               </Text>
             ) : (
-              <Box pl={8}>
+              <Box pl={9}>
                 <VulnTypes data={data?.sbom} />
               </Box>
             )}
