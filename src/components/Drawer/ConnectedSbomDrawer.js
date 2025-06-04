fa72724e7a3d48580fc59394846f545684ca950e
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2025-06-04 17:08:25 +0530

    Refactor connected SBOM drawer (#6485)

--- src/components/Drawer/ConnectedSbomDrawer.js
+++ src/components/Drawer/ConnectedSbomDrawer.js
@@ -1,84 +1,95 @@
 import { statusColor } from 'utils/styleUtils'
 
-import {
-  Table,
-  Tag,
-  TagLabel,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr
-} from '@chakra-ui/react'
+import { Tag, TagLabel, Text } from '@chakra-ui/react'
 
 import LynkDrawer from 'components/LynkDrawer'
+import LynkTable from 'components/LynkTable'
 
-const TABLE_HEADERS = ['PRODUCT', 'VERSION', 'ENVIRONMENT', 'STATUS']
+import { useThemeColor } from 'hooks/useThemeColors'
 
 const ConnectedSbomDrawer = ({ data, isOpen, onClose }) => {
+  const { primaryTextColor } = useThemeColor(['primaryTextColor'])
+
   const connectedSboms = data?.connectedSboms || []
+
+  const columns = [
+    {
+      id: 'PRODUCT',
+      name: 'PRODUCT',
+      compact: true,
+      wrap: true,
+      selector: (row) => {
+        const { project } = row || {}
+        return (
+          <Text fontSize={14} color={primaryTextColor}>
+            {project?.projectGroup?.name || 'N/A'}
+          </Text>
+        )
+      }
+    },
+    {
+      id: 'VERSION',
+      name: 'VERSION',
+      wrap: true,
+      selector: (row) => {
+        const { projectVersion } = row || {}
+        return (
+          <Text fontSize={14} color={primaryTextColor}>
+            {projectVersion || 'N/A'}
+          </Text>
+        )
+      }
+    },
+    {
+      id: 'ENVIRONMENT',
+      name: 'ENVIRONMENT',
+      wrap: true,
+      selector: (row) => {
+        const { project } = row || {}
+        return (
+          <Text fontSize={14} color={primaryTextColor}>
+            {project?.name || 'N/A'}
+          </Text>
+        )
+      }
+    },
+    {
+      id: 'STATUS',
+      name: 'STATUS',
+      right: true,
+      wrap: true,
+      selector: (row) => {
+        const { parentDispositionFrom } = row || {}
+        const { vexStatus } = parentDispositionFrom || {}
+        return (
+          <Tag
+            size='md'
+            variant='solid'
+            width={'130px'}
+            colorScheme={statusColor(vexStatus?.name || 'Unspecified')}
+          >
+            <TagLabel style={{ textTransform: 'capitalize' }} mx={'auto'}>
+              {vexStatus?.name || 'Unspecified'}
+            </TagLabel>
+          </Tag>
+        )
+      }
+    }
+  ]
 
   return (
     <LynkDrawer
-      title={'Also Affected'}
       size='xl'
+      noFooter
       isOpen={isOpen}
       onClose={onClose}
-      noFooter
+      title={'Also Affected'}
     >
-      <Table variant='simple' m={0} p={0}>
-        <Thead>
-          <Tr>
-            {TABLE_HEADERS.map((item, index) => (
-              <Th
-                px={0}
-                key={index}
-                textAlign={item === 'STATUS' ? 'right' : 'left'}
-              >
-                {item}
-              </Th>
-            ))}
-          </Tr>
-        </Thead>
-        <Tbody width={'100%'}>
-          {connectedSboms.length > 0 &&
-            connectedSboms.map((item, index) => {
-              const { project, projectVersion } = item
-              return (
-                <Tr key={index}>
-                  <Td pl={0} fontSize={'sm'} wordBreak={'break-all'}>
-                    {project?.projectGroup?.name || ''}
-                  </Td>
-                  <Td pl={0} fontSize={'sm'} wordBreak={'break-all'}>
-                    {projectVersion || ''}
-                  </Td>
-                  <Td pl={0} fontSize={'sm'} wordBreak={'break-all'}>
-                    {project?.name || ''}
-                  </Td>
-                  <Td pr={0} fontSize={'sm'} textAlign={'right'}>
-                    <Tag
-                      size='md'
-                      variant='solid'
-                      width={'130px'}
-                      colorScheme={statusColor(
-                        item?.parentDispositionFrom?.vexStatus?.name ||
-                          'Unspecified'
-                      )}
-                    >
-                      <TagLabel
-                        style={{ textTransform: 'capitalize' }}
-                        mx={'auto'}
-                      >
-                        {item?.parentDispositionFrom?.vexStatus?.name ||
-                          'Unspecified'}
-                      </TagLabel>
-                    </Tag>
-                  </Td>
-                </Tr>
-              )
-            })}
-        </Tbody>
-      </Table>
+      <LynkTable
+        columns={columns}
+        data={connectedSboms}
+        className='data-table-container'
+      />
     </LynkDrawer>
   )
 }
