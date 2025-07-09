1398c5c8190131c971670357b307bf79999976bf
Abhisek Paul <135737557+abhisekdev@users.noreply.github.com>
2025-07-09 20:06:19 +0530

    Fix product details test (#6867)

--- playwright-test/selectors/product_details.selector.ts
+++ playwright-test/selectors/product_details.selector.ts
@@ -14,9 +14,9 @@ class ProductDetailsSelectors {
   public static uploadBtn =
     "//*[@id='root']/div/div[2]/div[2]/div/div[1]/div/div/div[2]/div/div[2]/button[2]"
   public static uplaodPopup =
-    "//section[contains(@class,'chakra-modal__content')]//form"
+    "//section[contains(@class,'chakra-modal__content')]"
   public static uploadPopupHeader =
-    "//section[contains(@class,'chakra-modal__content')]//form//p[text()='Upload SBOM']"
+    "//section[contains(@class,'chakra-modal__content')]//p[text()='Upload SBOM']"
   public static uploadFileInput = "//input[@id='fileInput']"
   public static upload = "//button[@type='submit']"
   public static uploadSuccessMsg =
@@ -36,7 +36,7 @@ class ProductDetailsSelectors {
   }
   public static deleteBtn = '[data-testid="sbom-delete-button"]'
   public static deletePopup =
-    "//section[contains(@class,'chakra-modal__content')]//form"
+    "//section[contains(@class,'chakra-modal__content')]"
   public static yesBtn = "//button[text()='Yes']"
   public static versionSearch = "input[name='versions']"
   public static pNameHeader(name: string) {
