a21ce8c0170d531315c38e882af03e98753e9eb5
Ajay Jose <62168620+ajayjoseaji@users.noreply.github.com>
2025-07-16 10:41:47 +0530

    chore: fix typos, improve accessibility, and clean up jsx (#6942)

--- src/components/PolicyTerms.js
+++ src/components/PolicyTerms.js
@@ -9,22 +9,31 @@ const PolicyTerms = () => {
     'sameSecondaryText',
     'primaryBlueText'
   ])
+
   return (
     <Stack>
-      <Text fontSize={'xs'} color={sameSecondaryText} textAlign={'center'}>
-        By logging in, you acknowledge that you have read and agree to the
+      <Text fontSize='xs' color={sameSecondaryText} textAlign='center'>
+        By logging in, you acknowledge that you have read and agree to the{' '}
         <chakra.span color={primaryBlueText}>
-          {' '}
-          <Link target='_blank' to={'https://www.interlynk.io/privacy'}>
-            Privary Policy
+          <Link
+            to='https://www.interlynk.io/privacy'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Privacy Policy
           </Link>
         </chakra.span>{' '}
         and{' '}
         <chakra.span color={primaryBlueText}>
-          <Link target='_blank' to={'https://www.interlynk.io/terms'}>
+          <Link
+            to='https://www.interlynk.io/terms'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             Terms of Service
           </Link>
         </chakra.span>
+        .
       </Text>
     </Stack>
   )
